aa1e6485647ec7c02e428bb22f09dcc56e5a214f
cutsea110 <cutsea110@gmail.com>
2017-09-12 21:17:02 +0900

    add Tree

---
+++ Tree.hs
@@ -0,0 +1,19 @@
+module Tree where
+
+data Tree a = Tip a | Bin (Tree a) (Tree a) deriving (Show, Eq)
+
+foldt (f, g) (Tip a) = f a
+foldt (f, g) (Bin tl tr) = g (foldt (f, g) tl) (foldt (f, g) tr)
+
+unfoldt phi x = case phi x of
+  Left a -> Tip a
+  Right (tl, tr) -> Bin (unfoldt phi tl) (unfoldt phi tr)
+
+gen = unfoldt phi
+  where
+    phi n = if n <= 0 then Left n else Right (n-1, n-1)
+
+mapt f (Tip a) = Tip (f a)
+mapt f (Bin l r) = Bin (mapt f l) (mapt f r)
+
+mapt' f = foldt (Tip . f, Bin <$> id <$> id)
