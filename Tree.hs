a9d5502c86d77b58d8f9c34c5074f8d127ae13f1
cutsea110 <cutsea110@gmail.com>
2017-09-15 17:46:03 +0900

    update

--- Tree.hs
+++ Tree.hs
@@ -1,26 +1,32 @@
 module Tree where
 
-data Tree a = Tip a | Bin (Tree a) (Tree a) deriving (Show, Eq)
+data Tree a = Tip a | Bin (Tree a, Tree a) deriving (Show, Eq)
 
 foldt (f, g) (Tip a) = f a
-foldt (f, g) (Bin tl tr) = g (foldt (f, g) tl) (foldt (f, g) tr)
+foldt (f, g) (Bin (tl, tr)) = g (foldt (f, g) tl, foldt (f, g) tr)
 
 unfoldt phi x = case phi x of
   Left a -> Tip a
-  Right (tl, tr) -> Bin (unfoldt phi tl) (unfoldt phi tr)
+  Right (tl, tr) -> Bin (unfoldt phi tl, unfoldt phi tr)
 
 gen = unfoldt phi
   where
     phi n = if n <= 0 then Left n else Right (n-1, n-1)
 
 mapt f (Tip a) = Tip (f a)
-mapt f (Bin l r) = Bin (mapt f l) (mapt f r)
+mapt f (Bin (l, r)) = Bin (mapt f l, mapt f r)
 
 mapt' f = foldt (Tip . f, Bin <$> id <$> id)
 
-tips = foldt (wrap, cat)
-  where
-    cat x y = x ++ y
-    wrap x = [x]
+cat (x, y) = x ++ y
+wrap x = [x]
+cons (x, xs) = x:xs
 
-tips' t = foldt ((:), (.)) t []
+tips = foldt (wrap, cat)
+
+tipcat = curry cat . tips
+
+
+tips' t = foldt (curry cons, compose) t []
+
+compose (h, k) = h . k
