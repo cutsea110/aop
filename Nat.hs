5620dc022a807f6aa6ac31889e3a18e2bea0d409
cutsea110 <cutsea110@gmail.com>
2017-09-28 09:45:31 +0900

    update

--- Nat.hs
+++ Nat.hs
@@ -1,3 +1,5 @@
+import Prelude hiding (succ)
+
 data Nat = Zero
          | Succ Nat
          deriving (Show, Eq)
@@ -10,3 +12,21 @@ unfoldn :: (a -> Maybe a) -> a -> Nat
 unfoldn phi x = case phi x of
   Nothing -> Zero
   Just x' -> Succ (unfoldn phi x')
+
+toNat = unfoldn (\n -> if n <= 0 then Nothing else Just (n-1))
+
+succ = Succ
+succ' (Succ n) = n
+
+-- coreflexive over (Succ n) but Zero
+positive = succ . succ'
+
+-- coreflexive over Zero but (Succ n)
+corefZero = foldn (Zero, positive)
+
+-- const Zero
+constZero = foldn (Zero, id)
+
+-- what?
+-- the converse of corefZero
+corefZero' = undefined
