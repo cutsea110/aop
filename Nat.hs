489602fe8d099c83cd43798d6694e0ca5e67f866
cutsea110 <cutsea110@gmail.com>
2017-09-28 09:36:14 +0900

    update

---
+++ Nat.hs
@@ -0,0 +1,12 @@
+data Nat = Zero
+         | Succ Nat
+         deriving (Show, Eq)
+
+foldn :: (a, a -> a) -> Nat -> a
+foldn (c, f) Zero = c
+foldn (c, f) (Succ n) = f (foldn (c, f) n)
+
+unfoldn :: (a -> Maybe a) -> a -> Nat
+unfoldn phi x = case phi x of
+  Nothing -> Zero
+  Just x' -> Succ (unfoldn phi x')
