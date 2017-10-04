d1a22b023da493248db59a998d6449ac3d1e67d5
cutsea110 <cutsea110@gmail.com>
2017-10-04 23:15:26 +0900

    NonEmptyList
    NonEmptyList

---
+++ NonEmptyList.hs
@@ -0,0 +1,66 @@
+module NonEmptyList where
+
+import Prelude hiding (foldr, unfoldr, sum, length)
+
+-- L = 1 + A x L
+data List a = Nil | Cons a (List a) deriving Show
+
+foldr :: (b, a -> b -> b) -> List a -> b
+foldr (c, f) Nil = c
+foldr (c, f) (Cons x xs) = f x (foldr (c, f) xs)
+
+unfoldr :: (b -> Maybe (a, b)) -> b -> List a
+unfoldr phi xs = case phi xs of
+  Nothing -> Nil
+  Just (x, xs') -> Cons x (unfoldr phi xs')
+
+
+-- Identity
+cataId = foldr (Nil, Cons)
+anaId = unfoldr phi
+  where
+    phi Nil = Nothing
+    phi (Cons x xs) = Just (x, xs)
+
+-- Utility
+gen n = unfoldr phi n
+  where
+    phi n = if n <= 0 then Nothing else Just (n, n-1)
+
+----
+
+-- L^{+} = 1 + A x L
+data NonEmptyList a = Pair a (List a) deriving Show
+
+foldr' :: (t, a -> t -> t, a -> t -> t') -> NonEmptyList a -> t'
+foldr' (c, f, g) (Pair x xs) = g x (foldr (c, f) xs)
+
+unfoldr' :: (b -> Maybe (a, b), t -> (a, b)) -> t -> NonEmptyList a
+unfoldr' (phi, psi) xs = case psi xs of
+  (x, xs') -> Pair x (unfoldr phi xs')
+
+-- Identity
+cataId' = foldr' (Nil, Cons, Pair)
+anaId' = unfoldr' (phi, psi)
+  where
+    phi Nil = Nothing
+    phi (Cons x xs) = Just (x, xs)
+    psi (Pair x xs) = (x, xs)
+
+-- Utility
+gen' n = unfoldr' (phi, psi) n
+  where
+    phi n = if n <= 0 then Nothing else Just (n, n-1)
+    psi n = (n, n-1)
+
+------------------
+
+length :: List a -> Int
+length = foldr (0, const (1+))
+sum :: Num a => List a -> a
+sum = foldr (0, (+))
+
+length' :: NonEmptyList a -> Int
+length' = foldr' (0, const (1+), const (1+))
+sum' :: Num a => NonEmptyList a -> a
+sum' = foldr' (0, (+), (+))
