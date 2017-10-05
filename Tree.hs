9872f5777f9218464473efb69b3275f329092ac1
cutsea110 <cutsea110@gmail.com>
2017-10-06 07:31:39 +0900

    uncurried ver

--- Tree.hs
+++ Tree.hs
@@ -1,22 +1,26 @@
 module Tree where
 
-data Tree a = Tip a | Bin (Tree a, Tree a) deriving (Show, Eq)
+cross (f, g) (x, y) = (f x, g y)
+
+data Tree a = Tip a | Bin (Tree a) (Tree a) deriving (Show, Eq)
+tip = Tip
+bin = uncurry Bin
 
 foldt (f, g) (Tip a) = f a
-foldt (f, g) (Bin (tl, tr)) = g (foldt (f, g) tl, foldt (f, g) tr)
+foldt (f, g) (Bin tl tr) = g (foldt (f, g) tl, foldt (f, g) tr)
 
 unfoldt phi x = case phi x of
   Left a -> Tip a
-  Right (tl, tr) -> Bin (unfoldt phi tl, unfoldt phi tr)
+  Right (tl, tr) -> bin (unfoldt phi tl, unfoldt phi tr)
 
 gen = unfoldt phi
   where
     phi n = if n <= 0 then Left n else Right (n-1, n-1)
 
-mapt f (Tip a) = Tip (f a)
-mapt f (Bin (l, r)) = Bin (mapt f l, mapt f r)
+mapt f (Tip a) = tip (f a)
+mapt f (Bin l r) = bin (mapt f l, mapt f r)
 
-mapt' f = foldt (Tip . f, Bin <$> id <$> id)
+mapt' f = foldt (tip . f, bin . cross (id, id))
 
 cat (x, y) = x ++ y
 wrap x = [x]
