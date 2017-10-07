2282586d395dd27ad1d79ccf776b218e6e2f0689
cutsea110 <cutsea110@gmail.com>
2017-10-07 20:15:41 +0900

    update

--- List.hs
+++ List.hs
@@ -12,8 +12,13 @@ compose (f, g) = f . g
 nil = Nil
 cons = uncurry Cons
 
+foldr :: (b, (a, b) -> b) -> Cons a -> b
 foldr (c, f) Nil = c
 foldr (c, f) (Cons x xs) = f (x, foldr (c, f) xs)
+
+cfoldr :: (b, a -> b -> b) -> Cons a -> b
+cfoldr (c, f) Nil = c
+cfoldr (c, f) (Cons x xs) = f x (cfoldr (c, f) xs)
 
 unfoldr phi x = case phi x of
   Nothing -> nil
@@ -31,11 +36,13 @@ listr' f = foldr (Nil, cons . cross (f, id))
 cat (Nil, ys)       = ys
 cat (Cons x xs, ys) = cons (x, cat (xs, ys))
 
+ccat :: Cons a -> Cons a -> Cons a
 ccat Nil = id
 ccat (Cons a x) = (Cons a . ccat x)
 
--- ccat' = foldr (id, (.) <$> Cons <$> id)
-ccat' = foldr (id, compose . cross (Cons, id))
+ccat' :: Cons a -> Cons a -> Cons a
+ccat' Nil = id
+ccat' (Cons x xs) = compose (cross (Cons , ccat') (x, xs))
 
 len = foldr (0, plus1)
   where
