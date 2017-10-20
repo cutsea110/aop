180d876d45cc4c4ea6efb460051ae93b656c463f
cutsea110 <cutsea110@gmail.com>
2017-10-21 07:42:11 +0900

    BinTree

--- BinTree.hs
+++ BinTree.hs
@@ -1,12 +1,14 @@
 module BinTree where
 
-data Bin a = Bin a (Bin a) (Bin a) deriving Show
+data Bin a = Leaf | Bin a (Bin a) (Bin a) deriving Show
 
+cata (d, g) Leaf = d
+cata (d, g) (Bin x l r) = g x (cata (d, g) l) (cata (d, g) r)
 
-cata f (Bin x l r) = f x (cata f l) (cata f r)
-
-para g (Bin x l r) = g x (l, para g l) (r, para g r)
+para (d, g) Leaf = d
+para (d, g) (Bin x l r) = g x (l, para (d, g) l) (r, para (d, g) r)
 
 
 ana phi x = case phi x of
-  (x', l, r) -> Bin x' (ana phi l) (ana phi r)
+  Nothing -> Leaf
+  Just (x', l, r) -> Bin x' (ana phi l) (ana phi r)
