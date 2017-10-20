60399dad3e83ae0f3068a5024c3d4e8480fed256
cutsea110 <cutsea110@gmail.com>
2017-10-21 07:39:32 +0900

    BinTree

--- BinTree.hs
+++ BinTree.hs
@@ -2,6 +2,11 @@ module BinTree where
 
 data Bin a = Bin a (Bin a) (Bin a) deriving Show
 
+
 cata f (Bin x l r) = f x (cata f l) (cata f r)
 
-para f (Bin x l r) = f x (l, para f l) (r, para f r)
+para g (Bin x l r) = g x (l, para g l) (r, para g r)
+
+
+ana phi x = case phi x of
+  (x', l, r) -> Bin x' (ana phi l) (ana phi r)
