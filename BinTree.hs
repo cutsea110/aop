fc2dfd292a715248de407bb781344366b3d7ed68
cutsea110 <cutsea110@gmail.com>
2017-10-22 08:26:07 +0900

    generate bintree

--- BinTree.hs
+++ BinTree.hs
@@ -12,3 +12,8 @@ para (d, g) (Bin x l r) = g x (l, para (d, g) l) (r, para (d, g) r)
 ana phi x = case phi x of
   Nothing -> Leaf
   Just (x', l, r) -> Bin x' (ana phi l) (ana phi r)
+
+gen = ana phi
+  where
+    phi n = if n <= 1 then Nothing else Just (n, n-1, n-2)
+    
