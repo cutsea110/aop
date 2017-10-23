e5d0bebcb0ccb81a52abc47b3eb6b461f4be63d0
cutsea110 <cutsea110@gmail.com>
2017-10-23 10:13:37 +0900

    some functions

--- Fix.hs
+++ Fix.hs
@@ -16,26 +16,30 @@ data NatF x = Zero | Succ x deriving (Show, Functor)
 
 type Nat = Fix NatF
 
-instance Show Nat where
-  show n = "(" ++ show (out n) ++ ")"
-
-
 zero :: Nat
 zero = In Zero
 succ :: Nat -> Nat
 succ n = In (Succ n)
 
+instance Show Nat where
+  show n = "(" ++ show (out n) ++ ")"
+
 toInt :: Nat -> Int
 toInt = cata phi
   where
     phi Zero = 0
-    phi (Succ n) = 1 + n
+    phi (Succ n) = 1 + n -- !?
 
 double :: Nat -> Nat
 double = cata phi
   where
     phi Zero = zero
-    phi (Succ n) = succ (succ n)
+    phi (Succ n) = succ (succ n) -- !?
+
+fromInt :: Int -> Nat
+fromInt = ana psi
+  where
+    psi n = if n <= 0 then Zero else Succ (n - 1)
 
 -- | List a
 
@@ -43,13 +47,13 @@ data ListF a x = Nil | Cons a x deriving (Show, Functor)
 
 type List a = Fix (ListF a)
 
-instance Show a => Show (List a) where
-  show x = "(" ++ show (out x) ++ ")"
-
 nil :: List a
 nil = In Nil
 cons :: a -> List a -> List a
 cons x xs = In (Cons x xs)
+
+instance Show a => Show (List a) where
+  show x = "(" ++ show (out x) ++ ")"
 
 sum = cata phi
   where
@@ -60,3 +64,8 @@ length = cata phi
   where
     phi Nil = 0
     phi (Cons a x) = 1 + x -- !?
+
+genList :: Integer -> List Integer
+genList = ana psi
+  where
+    psi n = if n <= 0 then Nil else Cons n (n - 1)
