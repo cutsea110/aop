6723b2e2f9eb8f9af3d794927cbb3320d4c5018d
cutsea110 <cutsea110@gmail.com>
2017-10-25 10:35:35 +0900

    histo, futu, chrono, zygo, cozygo, dyna, codyna, mutu from Fix.hs

--- src/FixPrime.hs
+++ src/FixPrime.hs
@@ -1,12 +1,11 @@
 module FixPrime where
 
-import Prelude hiding (Functor(..), map, succ, either)
+import Prelude hiding (Functor(..), map, succ, either, head, last)
 
 pair (f, g) x = (f x, g x)
 cross (f, g) (x, y) = (f x, g y)
 either (f, g) (Left x) = f x
 either (f, g) (Right x) = g x
-
 
 class Bifunctor (f :: * -> * -> *) where
   bimap :: (a -> c, b -> d) -> f a b -> f c d
@@ -15,6 +14,15 @@ class Functor (f :: * -> *) where
   fmap :: (a -> b) -> f a -> f b
 
 newtype Fix f = In { out :: f (Fix f) }
+
+
+newtype HisF f a = His { unHis :: (a, f (HisF f a)) }
+head :: HisF f t -> t
+head = fst . unHis
+
+newtype FutF f a = Fut { unFut :: Either a (f (FutF f a)) }
+last :: a -> FutF f a
+last = Fut . Left
 
 -- catamorphism
 cata :: Functor f => (f a -> a) -> Fix f -> a
@@ -34,6 +42,30 @@ para phi = phi . fmap (pair (id, para phi)) . out
 -- apomorphism
 apo :: Functor f => (t -> f (Either (Fix f) t)) -> t -> Fix f
 apo psi = In . fmap (either (id, apo psi)) . psi
+-- histomorphism
+histo :: Functor f => (f (HisF f t) -> t) -> Fix f -> t
+histo phi = head . cata (His . pair (phi, id))
+-- futumorphism
+futu :: Functor f => (t -> f (FutF f t)) -> t -> Fix f
+futu psi = ana (either (psi, id) . unFut) . last
+-- chronomorphism
+chrono :: Functor f => (f (HisF f b) -> b) -> (a -> f (FutF f a)) -> a -> b
+chrono phi psi = histo phi . futu psi
+-- zygomorphism
+zygo :: Functor f => (f a -> a) -> (f (a, b) -> b) -> Fix f -> b
+zygo f phi = snd . cata (pair (f . fmap fst, phi))
+-- cozygomorphism
+cozygo :: Functor f => (a -> f a) -> (b -> f (Either a b)) -> b -> Fix f
+cozygo f psi = ana (either (fmap Left . f, psi)) . Right
+-- dynamorphism
+dyna :: Functor f => (f (HisF f b) -> b) -> (a -> f a) -> a -> b
+dyna f g = chrono f (fmap last . g)
+-- codynamorphism
+codyna :: Functor f => (f b -> b) -> (a -> f (FutF f a)) -> a -> b
+codyna f g = chrono (f . fmap head) g
+-- mutumorphism
+mutu :: Functor f => (a -> b) -> (f a -> a) -> Fix f -> b
+mutu proj phi = proj . cata phi
 -- type functor
 map :: (Bifunctor f, Functor (f a)) => (a -> c) -> Fix (f a) -> Fix (f c)
 map f = cata phi
