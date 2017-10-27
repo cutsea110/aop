cbad46541debaa0c521a46c0201e3db3111e3ccd
cutsea110 <cutsea110@gmail.com>
2017-10-27 09:44:19 +0900

    fib by hylo, intermediated type is call-tree

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Algebra of Programming by Haskell
 
 Fix.hs
   ref.) http://d.hatena.ne.jp/KeisukeNakano/20090320/1237559049
+
+Fib.hs
+  ref.) http://titech-ssr.blog.jp/archives/1047835805.html
+
---
+++ src/Fib.hs
@@ -0,0 +1,21 @@
+-- | ref.) http://titech-ssr.blog.jp/archives/1047835805.html
+module Fib where
+
+import Fix
+
+data FibT a = FZero | FOne | FNode a a
+
+instance Functor FibT where
+  fmap _ FZero = FZero
+  fmap _ FOne  = FOne
+  fmap f (FNode x y) = FNode (f x) (f y)
+
+fib = hylo phi psi
+  where
+    psi 0 = FZero
+    psi 1 = FOne
+    psi n = FNode (n-1) (n-2)
+    
+    phi FZero = 0
+    phi FOne  = 1
+    phi (FNode x y) = x + y
