8ca0307785e20c5b1d2b65e85987010b87aa2e21
cutsea110 <cutsea110@gmail.com>
2017-11-07 09:56:34 +0900

    [Refactor]

--- src/FixPrime.hs
+++ src/FixPrime.hs
@@ -4,7 +4,7 @@
 #-}
 module FixPrime where
 
-import Prelude hiding (Functor(..), map, succ, either, head, last)
+import Prelude hiding (Functor(..), map, succ, either)
 
 pair (f, g) x = (f x, g x)
 cross (f, g) (x, y) = (f x, g y)
@@ -125,67 +125,3 @@ mutu proj phi = proj . cata phi
 -- type functor
 map :: (Bifunctor f, Functor (f a)) => (a -> c) -> Fix (f a) -> Fix (f c)
 map f = cata (In . bimap (f, id))
-
--- | Natural Number
-data NatF a x = Zero | Succ a x deriving (Show)
-type Nat = Fix (NatF ())
-
-zero :: Nat
-zero = In Zero
-succ :: Nat -> Nat
-succ n = In (Succ () n)
-
-instance Show Nat where
-  show (In Zero) = "Zero"
-  show (In (Succ () (In Zero))) = "Succ Zero"
-  show (In (Succ () n)) = "Succ (" ++ show n ++ ")"
-
-instance Bifunctor NatF where
-  bimap (f, g) Zero = Zero
-  bimap (f, g) (Succ x y) = Succ (f x) (g y)
-
-instance Functor (NatF ()) where
-  fmap f Zero = Zero
-  fmap f (Succ () y) = Succ () (f y)
-
--- | List a
-data ListF a x = Nil | Cons a x deriving (Show)
-type List a = Fix (ListF a)
-
-nil :: List a
-nil = In Nil
-cons :: a -> List a -> List a
-cons x xs = In (Cons x xs)
-
-instance Show a => Show (List a) where
-  show x = "(" ++ show (out x) ++ ")"
-
-instance Bifunctor ListF where
-  bimap (f, g) Nil = Nil
-  bimap (f, g) (Cons a x) = Cons (f a) (g x)
-
-instance Functor (ListF a) where
-  fmap f Nil = Nil
-  fmap f (Cons a x) = Cons a (f x)
-
--- | Tree a
-data TreeF a x = Tip a | Bin x x deriving (Show)
-type Tree a = Fix (TreeF a)
-
-tip :: a -> Tree a
-tip = In . Tip
-bin :: Tree a -> Tree a -> Tree a
-bin l r = In (Bin l r)
-
-instance Show a => Show (Tree a) where
-  show (In (Tip x)) = "Tip " ++ show x
-  show (In (Bin l r)) = "Bin (" ++ show l ++ ") (" ++ show r ++ ")"
-
-instance Bifunctor TreeF where
-  bimap (f, g) (Tip x) = Tip (f x)
-  bimap (f, g) (Bin l r) = Bin (g l) (g r)
-
-instance Functor (TreeF a) where
-  fmap f (Tip x) = Tip x
-  fmap f (Bin l r) = Bin (f l) (f r)
-  
