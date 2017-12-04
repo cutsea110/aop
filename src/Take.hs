d77a107d6375d6eafb01dceed78d321df879094c
cutsea110 <cutsea110@gmail.com>
2017-12-04 11:37:03 +0900

    flip args

--- src/Take.hs
+++ src/Take.hs
@@ -16,11 +16,11 @@ plus n = foldn (n, S)
 mult n = foldn (Z, plus n)
 expr n = foldn (S Z, mult n)
 
-paran :: (a, (a, Nat) -> a) -> Nat -> a
+paran :: (a, (Nat, a) -> a) -> Nat -> a
 paran (c, g) = u
   where u = \case
           Z -> c
-          S n -> g (u n, n)
+          S n -> g (n, u n)
 
 fact = paran (S Z, f)
   where f (m, n) = mult m (S n)
