b3d0874c75a2b27f2a6ddcdf3955b59e4407f923
cutsea110 <cutsea110@gmail.com>
2018-03-21 07:47:49 +0900

    [Refactor] remove comment

--- src/Ack.hs
+++ src/Ack.hs
@@ -1,21 +1,6 @@
 {-# LANGUAGE NPlusKPatterns #-}
 module Ack where
 
-{--
-ack (Zero, y) = Succ y
-ack (Succ x, Zero) = ack (x, Succ Zero)
-ack (Succ x, Succ y) = ack (x, ack (Succ x, y))
-
-ack' = foldn (Succ, f)
-  where f g = foldn (g (Succ Zero), g)
-
-ack'' = foldn (Succ, swap f)
-  where f = foldn (ap1, ap2)
-        ap1 g = g (Succ Zero)
-        ap2 g h = h (g h)
-        swap f a b = f b a
---}
-
 cata :: (a, a -> a) -> Int -> a
 cata (c, f) 0 = c
 cata (c, f) (n+1) = f (cata (c, f) n)
