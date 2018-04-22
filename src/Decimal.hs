10e3cfe2dc08bc3c2d1dbf48615a0152e8c34f52
cutsea110 <cutsea110@gmail.com>
2018-04-23 08:05:16 +0900

    implement Digit and Digit+ on Int

--- src/Decimal.hs
+++ src/Decimal.hs
@@ -6,9 +6,20 @@ module Decimal where
 import FixPrime
 
 -- Digit = {0,1,2,3,4,5,6,7,8,9}
-newtype Digit = D { unD :: Int } deriving Show
+newtype Digit = D { unD :: Int }
+instance Show Digit where
+    show = show . unD
+d :: Int -> Digit
+d n | 0 <= n && n < 10 = D n
+    | otherwise = error "invalid Digit value"    
+
 -- Digit+ = {1,2,3,4,5,6,7,8,9}
-newtype DigitPlus = DP { unDP :: Int } deriving Show
+newtype DigitPlus = DP { unDP :: Int }
+instance Show DigitPlus where
+    show = show . unDP
+dp :: Int -> DigitPlus
+dp n | 0 < n && n < 10 = DP n
+     | otherwise = error "invalid Digit+ value"
 
 data DecimalF a x = Wrap DigitPlus | Snoc (x, a) deriving Show
 type Decimal = Fix (DecimalF Digit)
