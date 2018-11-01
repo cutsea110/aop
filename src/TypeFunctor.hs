41acbd630ac47955ad34134385e7621a0f7bfeaf
cutsea110 <cutsea110@gmail.com>
2018-11-01 14:46:12 +0900

    for tree

--- src/TypeFunctor.hs
+++ src/TypeFunctor.hs
@@ -9,7 +9,7 @@ data List a = Nil
             | Cons a (List a)
             deriving (Show, Eq)
 
-cata (c, f) = u
+fold (c, f) = u
   where
     u Nil = c
     u (Cons x xs) = f (x, u xs)
@@ -17,6 +17,22 @@ cata (c, f) = u
 nil = Nil
 cons = uncurry Cons
 
--- listr f = cata (alpha . F(f, id))
---         = cata ([nil, cons] . (id + f * id))
-listr f = cata (nil, cons . cross (f, id))
+-- list f = fold (alpha . F(f, id))
+--        = fold ([nil, cons] . (id + f * id))
+list f = fold (nil, cons . cross (f, id))
+
+data Tree a = Tip a
+            | Bin (Tree a) (Tree a)
+            deriving (Show, Eq)
+
+tip = Tip
+bin = uncurry Bin
+
+foldt (f, g) = u
+  where
+    u (Tip a) = f a
+    u (Bin l r) = g (u l, u r)
+
+-- tree f = foldt (alpha . F(f, id))
+--        = foldt ([tip, bin] . (f + id * id))
+tree f = foldt (tip . f, bin . cross (id, id))
