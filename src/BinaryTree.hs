af5faa7f0149caa8ee3a044ee0131b8cc6c63d1d
cutsea110 <cutsea110@gmail.com>
2018-11-05 11:10:40 +0900

    Binary Tree which has no good monad

---
+++ src/BinaryTree.hs
@@ -0,0 +1,16 @@
+module BinaryTree where
+
+cross3 (f, g, h) (x, y, z) = (f x, g y, h z)
+
+data BTree1 a = Empty
+              | Node a (BTree1 a) (BTree1 a)
+              deriving (Show, Eq)
+
+empty = Empty
+node (x, l, r) = Node x l r
+
+foldbt1 (c, f) Empty = c
+foldbt1 (c, f) (Node x l r) = f (x, foldbt1 (c, f) l, foldbt1 (c, f) r)
+
+mapbt1 :: (a -> b) -> BTree1 a -> BTree1 b
+mapbt1 f = foldbt1 (empty, node . cross3 (f, id, id))
