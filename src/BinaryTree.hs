5872674eedf3c40c4c64bbcbed9f0ebca83864ae
cutsea110 <cutsea110@gmail.com>
2018-11-05 11:20:54 +0900

    what is mu?

--- src/BinaryTree.hs
+++ src/BinaryTree.hs
@@ -14,3 +14,11 @@ foldbt1 (c, f) (Node x l r) = f (x, foldbt1 (c, f) l, foldbt1 (c, f) r)
 
 mapbt1 :: (a -> b) -> BTree1 a -> BTree1 b
 mapbt1 f = foldbt1 (empty, node . cross3 (f, id, id))
+
+eta x = node (x, empty, empty)
+mu = foldbt1 (empty, f)
+  where
+    f (Empty, Empty, Empty) = empty
+    f (Node x l r, ls, rs) = undefined
+    f (Empty, Node x l r, rs) = undefined
+    f (Empty, Empty, Node x l r) = undefined
