74102e47e774620939d8ea169dcbb58af83dc120
cutsea110 <cutsea110@gmail.com>
2018-11-09 10:06:40 +0900

    Binary Tree

--- src/Free.hs
+++ src/Free.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE FlexibleContexts, UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts, UndecidableInstances, TypeSynonymInstances, FlexibleInstances #-}
 -- Ref.) https://stackoverflow.com/questions/13352205/what-are-free-monads/13352580
 module Free where
 
-{--
+
 data Free f a = Pure a
               | Roll (f (Free f a))
 
@@ -23,8 +23,29 @@ instance Functor f => Applicative (Free f) where
 instance (Functor f, Applicative (Free f)) => Monad (Free f) where
     return = pure
     x >>= f = concatFree (f <$> x)
---}
 
+data B a = B a a deriving Show
+
+instance Functor B where
+    fmap f (B x y) = B (f x) (f y)
+
+type Tree a = Free B a
+
+instance Show a => Show (Tree a) where
+    show (Pure a) = "Pure " ++ show a
+    show (Roll (B x y)) = "Roll (B {" ++ show x ++ "," ++ show y ++ "})" 
+
+-- >>> let tr = Roll (B (Pure 2) (Pure 1))
+-- >>> let tl = Roll (B (Pure 4) (Pure 3))
+-- >>> let t = Roll (B (Pure 5) (Roll (B tl tr))
+-- >>> let f1 = Pure (*2)
+-- >>> fmap (*2) t
+-- >>> f1 <*> t
+-- >>> let f2 = Roll (B (Pure (*2)) (Pure (+2)))
+-- >>> f2 <*> tl
+-- >>> f2 <*> t
+
+{--
 -- the case of f is Identity
 -- F(A, TA) = A + TA
 data Free a = Pure a
@@ -69,3 +90,4 @@ mu (Roll x) = Roll (mu x)
 instance Monad Free where
     return = pure
     x >>= f = mu (f <$> x)
+--}
