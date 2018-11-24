66655e8f5a6caff07fe5ca06bdd6ea8b1549cc90
cutsea110 <cutsea110@gmail.com>
2018-11-24 13:44:50 +0900

    implement subseqs

--- src/Combinatorial.hs
+++ src/Combinatorial.hs
@@ -1,9 +1,7 @@
 module Combinatorial where
 
-data List a = Nil | Cons a (List a) deriving (Show, Eq)
-
-cata (c, f) Nil = c
-cata (c, f) (Cons x xs) = f (x, cata (c, f) xs)
-
-listr f Nil = Nil
-listr f (Cons x xs) = Cons (f x) (listr f xs)
+subseqs :: [a] -> [[a]]
+subseqs = foldr f e
+    where
+        e = [[]]
+        f a xs = [a:x | x <- xs] ++ xs
