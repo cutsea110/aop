896785037f9628319be9dc2e8f67a371f657fca9
cutsea110 <cutsea110@gmail.com>
2018-11-24 14:11:17 +0900

    trace book's code

--- src/Combinatorial.hs
+++ src/Combinatorial.hs
@@ -1,7 +1,27 @@
 module Combinatorial where
-
+{--
 subseqs :: [a] -> [[a]]
 subseqs = foldr f e
     where
         e = [[]]
         f a xs = [a:x | x <- xs] ++ xs
+--}
+
+cata (c, f) = foldr (curry f) c
+outl (x, _) = x
+outr (_, y) = y
+cpr (a, x) = [(a, b) | b <- x]
+wrap x = [x]
+nil = []
+cat (x, y) = x ++ y
+pair (f, g) x = (f x, g x)
+cross (f, g) (x, y) = (f x, g y)
+cons (x, xs) = x : xs
+list = map
+
+subseqs :: [a] -> [[a]]
+subseqs = cata (e, f)
+    where
+        e = wrap nil
+        f = cat . pair (list cons . cpr, outr)
+
