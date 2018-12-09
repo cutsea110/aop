8677e653ef9c7dd394e6303ba733fe35e331fd9d
cutsea110 <cutsea110@gmail.com>
2018-12-10 06:17:05 +0900

    [FIX] map

--- src/Rose.hs
+++ src/Rose.hs
@@ -1,7 +1,7 @@
 module Rose where
 
-pair (f, g) (x, y) = (f x, g y)
-cross (f, g) x = (f x, g x)
+pair (f, g) x = (f x, g x)
+cross (f, g) (x, y) = (f x, g y)
 
 data Tree a = Node a (Forest a) deriving Show
 data Forest a = Nil | Cons (Tree a) (Forest a) deriving Show
@@ -17,3 +17,11 @@ foldf (g, c, h) (Cons t f) = h (foldt (g, c, h) t, foldf (g, c, h) f)
 mapt f (Node a fs) = node (f a, mapf f fs)
 mapf f Nil = nil
 mapf f (Cons t fs) = cons (mapt f t, mapf f fs)
+
+(mapt', mapf') = (genMap foldt, genMap foldf)
+  where
+    genMap cata f = cata (g, c, h)
+      where
+        g = node . cross (f, id)
+        c = nil
+        h = cons . cross (id, id)
