4f0ba82116d57149f7259409add833e8aeee226a
cutsea110 <cutsea110@gmail.com>
2019-01-23 06:59:05 +0900

    revert a little bit performance tune

--- src/OpenSet.hs
+++ src/OpenSet.hs
@@ -4,12 +4,6 @@ import Data.List as List ((\\))
 import Data.Set as Set
 
 
-isOpen x = all (==True) ok
-    where
-        ps = Set.filter (\(a, b) -> a < b) $ cartesianProduct x x
-        ps' = Set.map (\(a, b) -> (a `intersection` b, a `union` b)) ps
-        ok = toList $ Set.map (\(a, b) -> a `member` x && b `member` x) ps'
-
 openSets x = toList $ Set.map (toList.(Set.map toList)) $ Set.filter isOpen candidates
     where
         (e, u) = (fromList [], fromList x)
@@ -17,7 +11,14 @@ openSets x = toList $ Set.map (toList.(Set.map toList)) $ Set.filter isOpen cand
         pu = powerSet u
         candidates = Set.map (union conpact) $ powerSet (pu Set.\\ conpact)
         sub = Prelude.map toList $ toList (pu Set.\\ conpact)
-
+        isOpen x = all (==True) ok
+            where
+                x' = x Set.\\ conpact
+                ps = Set.filter (\(a, b) -> a < b) $ cartesianProduct x' x'
+                ps' = Set.map (\(a, b) -> (a `intersection` b, a `union` b)) ps
+                ok = toList $ Set.map (\(a, b) -> a `member` x && b `member` x) ps'
+    
+    
 main = mapM_ go $ zip [1..] $ openSets [0,1,2,3]
     where
         go (i, ln) = putStrLn $ show i ++ " : " ++ show ln
