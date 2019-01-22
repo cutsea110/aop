413b2c662f99541ebba1c5c56eb435197eb31002
cutsea110 <cutsea110@gmail.com>
2019-01-22 14:22:18 +0900

    openSet by Data.Set

--- src/OpenSet.hs
+++ src/OpenSet.hs
@@ -1,5 +1,6 @@
 module OpenSet where
 
+import Data.List as List ((\\))
 import Data.Set as Set
 
 e :: Set Int
@@ -8,11 +9,20 @@ u :: Set Int
 u = fromList [0,1,2]
 conpact = fromList [e, u]
 
+
 u' = powerSet u
+candidates = Set.map (union conpact) $ powerSet (u' Set.\\ conpact)
+sub = Prelude.map toList $ toList (u' Set.\\ conpact)
 
-candidates = Set.map (union conpact) $ powerSet (u' \\ conpact)
-
-openSets = Set.filter pred candidates
+isOpen x = all (==True) ok
     where
-        pred :: Set (Set Int) -> Bool
-        pred = undefined
+        x' = x Set.\\ conpact
+        ps = Set.filter (\(a, b) -> a < b) $ cartesianProduct x' x'
+        ps' = Set.map (\(a, b) -> (a `intersection` b, a `union` b)) ps
+        ok = toList $ Set.map (\(a, b) -> a `member` x && b `member` x) ps'
+
+openSets = Set.filter isOpen candidates
+
+openSets' = toList $ Set.map (toList.(Set.map toList)) openSets
+
+main = mapM_ print openSets'
