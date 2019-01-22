393bbca7bc7e30fbe9275087acc8cfec120f27e8
cutsea110 <cutsea110@gmail.com>
2019-01-23 06:43:43 +0900

    [Refactor]

--- src/OpenSet.hs
+++ src/OpenSet.hs
@@ -3,26 +3,21 @@ module OpenSet where
 import Data.List as List ((\\))
 import Data.Set as Set
 
-e :: Set Int
-e = fromList []
-u :: Set Int
-u = fromList [0,1,2]
-conpact = fromList [e, u]
-
-
-u' = powerSet u
-candidates = Set.map (union conpact) $ powerSet (u' Set.\\ conpact)
-sub = Prelude.map toList $ toList (u' Set.\\ conpact)
 
 isOpen x = all (==True) ok
     where
-        x' = x Set.\\ conpact
-        ps = Set.filter (\(a, b) -> a < b) $ cartesianProduct x' x'
+        ps = Set.filter (\(a, b) -> a < b) $ cartesianProduct x x
         ps' = Set.map (\(a, b) -> (a `intersection` b, a `union` b)) ps
         ok = toList $ Set.map (\(a, b) -> a `member` x && b `member` x) ps'
 
-openSets = toList $ Set.map (toList.(Set.map toList)) $ Set.filter isOpen candidates
+openSets x = toList $ Set.map (toList.(Set.map toList)) $ Set.filter isOpen candidates
+    where
+        (e, u) = (fromList [], fromList x)
+        conpact = fromList [e, u]
+        pu = powerSet u
+        candidates = Set.map (union conpact) $ powerSet (pu Set.\\ conpact)
+        sub = Prelude.map toList $ toList (pu Set.\\ conpact)
 
-main = mapM_ go $ zip [1..] openSets
+main = mapM_ go $ zip [1..] $ openSets [0,1,2,3]
     where
         go (i, ln) = putStrLn $ show i ++ " : " ++ show ln
