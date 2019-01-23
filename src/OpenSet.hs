93e1562443ffdc245f768f9a0f5e460bef7740e5
cutsea110 <cutsea110@gmail.com>
2019-01-24 01:49:35 +0900

    [Refactor] type signature

--- src/OpenSet.hs
+++ src/OpenSet.hs
@@ -4,6 +4,7 @@ import Data.List as List ((\\))
 import Data.Set as Set
 
 
+openSets :: Ord a => [a] -> [[[a]]]
 openSets x = toList $ Set.map (toList.(Set.map toList)) $ Set.filter isOpen candidates
     where
         (e, u) = (fromList [], fromList x)
@@ -18,7 +19,7 @@ openSets x = toList $ Set.map (toList.(Set.map toList)) $ Set.filter isOpen cand
                 ps' = Set.map (\(a, b) -> (a `intersection` b, a `union` b)) ps
                 ok = toList $ Set.map (\(a, b) -> a `member` x && b `member` x) ps'
     
-    
-main = mapM_ go $ zip [1..] $ openSets [0,1,2,3]
+main :: IO ()    
+main = mapM_ go $ zip [1..] $ openSets [0,1,2]
     where
         go (i, ln) = putStrLn $ show i ++ " : " ++ show ln
