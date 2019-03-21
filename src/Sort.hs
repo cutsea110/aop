65dcaa56a1365b92f25ac1d15f65d57483a11ce3
cutsea110 <cutsea110@gmail.com>
2019-03-21 09:10:16 +0900

    for quick sort

--- src/Sort.hs
+++ src/Sort.hs
@@ -8,3 +8,14 @@ ssort = unfoldr phi
   where
     phi [] = Nothing
     phi xs = let r@(y, ys) = (minimum xs, xs \\ [y]) in Just r -- select
+
+data Tree a = Null | Fork (Tree a, a, Tree a) deriving (Show, Eq)
+
+foldt (c, f) = u
+  where
+    u Null = c
+    u (Fork (lt, n, rt)) = f (u lt, n, u rt)
+
+flatten = foldt (nil, join)
+nil = []
+join (x, a, y) = x ++ [a] ++ y
