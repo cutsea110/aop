a3814e1b53495db53ce1fc30ba4ca92cacf98441
cutsea110 <cutsea110@gmail.com>
2019-03-28 09:25:37 +0900

    step by step

--- src/Collatz.hs
+++ src/Collatz.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 module Collatz where
-
+{--
 newtype Fix f = In { out :: f (Fix f) }
 
 type Nat = Fix Maybe
@@ -78,7 +78,7 @@ cata phi = phi . fmap (cata phi) . out
 
 histo phi = ex . cata ap
   where
-    ap x = cast $ Ano (phi x) x
+    ap = cast . (Ano <$> phi <*> id)
     cast :: Functor f => Ano f a (Cf f a) -> Cf f a
     cast = Cf . In . bimap (id, unCf)
 
@@ -89,3 +89,43 @@ psi (Just n) = f1 n + f2 n
     f2 n = case sub n of
       Nothing -> 1
       Just n' -> ex n'
+--}
+
+-- We would like to do this!
+psi Nothing = (0, Nothing) -- 0
+-- psi (Just Nothing) = (1, Just Nothing) -- NOT this
+psi (Just Nothing) = (1, Just (0, Nothing)) -- 1
+psi (Just (Just Nothing)) = (1, Just (1, Just (0, Nothing))) -- 2
+psi (Just (Just (Just Nothing))) = (2, Just (1, Just (1, Just (0, Nothing)))) -- 3
+psi (Just (Just (Just (Just Nothing)))) = (3, Just (2, Just (1, Just (1, Just (0, Nothing))))) -- 4
+psi (Just (Just (Just (Just (Just Nothing))))) = (5, Just (3, Just (2, Just (1, Just (1, Just (0, Nothing)))))) -- 5
+psi (Just (Just (Just (Just (Just (Just Nothing)))))) = (8, Just (5, Just (3, Just (2, Just (1, Just (1, Just (0, Nothing))))))) -- 6
+
+{-- couldn't typable
+psi' Nothing = (0, Nothing)
+psi' (Just Nothing) = (1, Just (psi' Nothing))
+psi' (Just n) = case psi' n of
+  p@(f1, Just (f2, mv)) -> (f1 + f2, Just p)
+--}
+
+type Nat = Fix Maybe
+
+data Fix f = In { out :: f (Fix f) }
+
+newtype Ano = Ano (Integer, Maybe Ano)
+
+-- fit a type
+psi' :: Nat -> Ano
+psi' (In Nothing) = Ano (0, Nothing) -- 0
+psi' (In (Just (In Nothing))) = Ano (1, Just (Ano (0, Nothing))) -- 1
+psi' (In (Just (In (Just (In Nothing))))) = Ano (1, Just (Ano (1, Just (Ano (0, Nothing))))) -- 2
+
+-- done!
+psi'' (In Nothing) = Ano (0, Nothing)
+psi'' (In (Just p@(In Nothing))) = Ano (1, Just (psi'' p))
+psi'' (In (Just n)) = case psi'' n of
+  p@(Ano (f1, Just (Ano (f2, mv)))) -> Ano (f1 + f2, Just p)
+
+ex (Ano (x, _)) = x
+toNat 0 = In Nothing
+toNat n = In (Just (toNat (n-1)))
