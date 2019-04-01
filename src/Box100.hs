a26e0f0f04d3bed2d8e0942397c5344ede4db5fb
cutsea110 <cutsea110@gmail.com>
2019-04-01 11:28:15 +0900

    exs is too hairy

--- src/Box100.hs
+++ src/Box100.hs
@@ -28,25 +28,26 @@ instance ApplicativeBifunctor TreeF where
   biap (Tip f) (Tip x) = Tip (f x)
   biap (Bin f g) (Bin l r) = Bin (f l) (g r)
 
--- windUp :: [Tree a] -> Tree a
+windUp :: Num a => [Cofree (TreeF t) a] -> Cofree (TreeF t) a
 windUp []  = error "Ooops!"
 windUp [x] = x
 windUp xs  = windUp (windUp1 xs)
 
--- windUp1 :: [Tree a] -> [Tree a]
+windUp1 :: Num a => [Cofree (TreeF t) a] -> [Cofree (TreeF t) a]
 windUp1 xs = zipWith f xs (tail xs)
   where
     f l r = Cf (In (Hisx (extract l + extract r, Bin (unCf l) (unCf r))))
 
+tip' :: a -> Cofree (TreeF a) a
 tip' n = Cf (In (Hisx (n, Tip n)))
 
--- zipWind :: ([Tree a], [Tree a], [Tree a]) -> Tree a
+zipWind :: Num a => ([a], [Cofree (TreeF a) a], [a]) -> Cofree (TreeF a) a
 zipWind ([], cs, []) = windUp cs
 zipWind ([], cs, r:rs) = zipWind ([], (windUp1 (cs ++ [tip' r])), rs)
 zipWind (l:ls, cs, []) = zipWind (ls, (windUp1 ([tip' l] ++ cs)), [])
 zipWind (l:ls, cs, r:rs) = zipWind (ls, (windUp1 ([tip' l] ++ cs ++ [tip' r])), rs)
 
-
+mkNexus :: Num a => ([a], [a]) -> Cofree (TreeF a) a
 mkNexus (ls, rs) = zipWind (ls, [], rs)
 
 calc :: Num t => Tree t -> t
@@ -59,3 +60,16 @@ calc = histo psi
 rows,cols :: [Int]
 rows = [4,2,5,6,7,1,3,9,3,2]
 cols = [8,2,4,6,1,8,9,3,1,7]
+
+exs :: Cofree (TreeF t) a -> [a]
+exs (Cf (In (Hisx (n, Tip _)))) = [n]
+exs (Cf (In (Hisx (n, Bin l r)))) = n : (exs (Cf l) ++ exs (Cf r))
+
+{-
+λ> draw' 3 tabulation [1,2,3] [4,5,6]
+      4   5   6
+   ============
+ 1|   5  10  16
+ 2|   7  17  33
+ 3|  10  27  60
+-}
