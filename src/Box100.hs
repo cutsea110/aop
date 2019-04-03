08a6138a2d2600701e4352a7961826028b30f117
cutsea110 <cutsea110@gmail.com>
2019-04-04 07:05:31 +0900

    parameterise on binary operator

--- src/Box100.hs
+++ src/Box100.hs
@@ -37,29 +37,26 @@ tip' n = Cf (In (Hisx (n, Tip n)))
 binWith' :: (a -> a -> a) -> Cofree (TreeF t) a -> Cofree (TreeF t) a -> Cofree (TreeF t) a
 binWith' op l r = Cf (In (Hisx (extract l `op` extract r, Bin (unCf l) (unCf r))))
 
-bin' :: Num a => Cofree (TreeF t) a -> Cofree (TreeF t) a -> Cofree (TreeF t) a
-bin' = binWith' (+)
-
 winder :: ((a, b) -> c) -> (b, [a]) -> Maybe (c, (c, [a]))
 winder f (y, xxs) = case xxs of
   []     -> Nothing
   (x:xs) -> Just (y', (y', xs)) where y' = f (x, y)
 
-windCol :: Num a => (Cofree (TreeF t) a, [Cofree (TreeF t) a]) -> [Cofree (TreeF t) a]
-windCol = unfoldr (winder (uncurry bin'))
+windCol :: (a -> a -> a) -> (Cofree (TreeF t) a, [Cofree (TreeF t) a]) -> [Cofree (TreeF t) a]
+windCol = unfoldr . winder . uncurry . binWith'
 
-nexus :: Num a => ([a], [a]) -> [[Cofree (TreeF a) a]]
-nexus (cs, rs) = unfoldr psi (map tip' cs, map tip' rs)
+nexus :: (a -> a -> a) -> ([a], [a]) -> [[Cofree (TreeF a) a]]
+nexus op (cs, rs) = unfoldr psi (map tip' cs, map tip' rs)
   where
     psi (cs, []) = Nothing
-    psi (cs, r:rs) = Just (ps, (ps, rs)) where ps = windCol (r, cs)
+    psi (cs, r:rs) = Just (ps, (ps, rs)) where ps = windCol op (r, cs)
 
-calc :: Num a => ([a], [a]) -> [[a]]
-calc (cs, rs) = map (map extract) $ nexus (cs, rs)
+calc :: (a -> a -> a) -> ([a], [a]) -> [[a]]
+calc op (cs, rs) = map (map extract) $ nexus op (cs, rs)
 
 rows,cols :: [Int]
 rows = [4,2,5,6,7,1,3,9,3,2]
 cols = [8,2,4,6,1,8,9,3,1,7]
 
 main :: IO ()
-main = draw' 6 calc cols rows
+main = draw' 6 (calc (+)) cols rows
