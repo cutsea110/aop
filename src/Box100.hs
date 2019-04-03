c67c545aa2d2463176bb057ce4202357ce56be00
cutsea110 <cutsea110@gmail.com>
2019-04-03 21:43:59 +0900

    arrange argument

--- src/Box100.hs
+++ src/Box100.hs
@@ -31,16 +31,6 @@ instance ApplicativeBifunctor TreeF where
   biap (Tip f) (Tip x) = Tip (f x)
   biap (Bin f g) (Bin l r) = Bin (f l) (g r)
 
-windUp :: Num a => [Cofree (TreeF t) a] -> Cofree (TreeF t) a
-windUp []  = error "Ooops!"
-windUp [x] = x
-windUp xs  = windUp (windUp1 xs)
-
-windUp1 :: Num a => [Cofree (TreeF t) a] -> [Cofree (TreeF t) a]
-windUp1 xs = zipWith f xs (tail xs)
-  where
-    f l r = Cf (In (Hisx (extract l + extract r, Bin (unCf l) (unCf r))))
-
 tip' :: a -> Cofree (TreeF a) a
 tip' n = Cf (In (Hisx (n, Tip n)))
 
@@ -50,15 +40,6 @@ binWith' op l r = Cf (In (Hisx (extract l `op` extract r, Bin (unCf l) (unCf r))
 bin' :: Num a => Cofree (TreeF t) a -> Cofree (TreeF t) a -> Cofree (TreeF t) a
 bin' = binWith' (+)
 
-zipWind :: Num a => ([a], [Cofree (TreeF a) a], [a]) -> Cofree (TreeF a) a
-zipWind ([], cs, []) = windUp cs
-zipWind ([], cs, r:rs) = zipWind ([], (windUp1 (cs ++ [tip' r])), rs)
-zipWind (l:ls, cs, []) = zipWind (ls, (windUp1 ([tip' l] ++ cs)), [])
-zipWind (l:ls, cs, r:rs) = zipWind (ls, (windUp1 ([tip' l] ++ cs ++ [tip' r])), rs)
-
-mkNexus :: Num a => ([a], [a]) -> Cofree (TreeF a) a
-mkNexus (ls, rs) = zipWind (ls, [], rs)
-
 winder :: ((a, b) -> c) -> (b, [a]) -> Maybe (c, (c, [a]))
 winder f (y, xxs) = case xxs of
   []     -> Nothing
@@ -67,28 +48,18 @@ winder f (y, xxs) = case xxs of
 windCol :: Num a => (Cofree (TreeF t) a, [Cofree (TreeF t) a]) -> [Cofree (TreeF t) a]
 windCol = unfoldr (winder (uncurry bin'))
 
-nexus :: Num a => [a] -> [a] -> [[Cofree (TreeF a) a]]
-nexus cols rows = unfoldr psi (map tip' cols, map tip' rows)
+nexus :: Num a => ([a], [a]) -> [[Cofree (TreeF a) a]]
+nexus (cs, rs) = unfoldr psi (map tip' cs, map tip' rs)
   where
     psi (cs, []) = Nothing
     psi (cs, r:rs) = Just (ps, (ps, rs)) where ps = windCol (r, cs)
 
-calc' :: Num a => [a] -> [a] -> [[a]]
-calc' rs cs = map (map extract) $ nexus rs cs
+calc :: Num a => ([a], [a]) -> [[a]]
+calc (cs, rs) = map (map extract) $ nexus (cs, rs)
 
 rows,cols :: [Int]
 rows = [4,2,5,6,7,1,3,9,3,2]
 cols = [8,2,4,6,1,8,9,3,1,7]
 
-exs :: Fix (Hisx (TreeF t) a) -> [[a]]
-exs x = case out x of
-  Hisx (_, Tip _)   -> []
-  Hisx (_, Bin _ r) -> exs r ++ [exsL x]
-  where
-    exsL :: Fix (Hisx (TreeF t) a) -> [a]
-    exsL x = case out x of
-      Hisx (_, Tip _) -> []
-      Hisx (n, Bin l _) -> exsL l ++ [n]
-
-calc :: Num a => ([a], [a]) -> [[a]]
-calc = exs . unCf . mkNexus
+main :: IO ()
+main = draw' 6 calc cols rows
