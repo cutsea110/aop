fce4b6d56fca515aa4c9b892d4ab27e2531d8fd9
cutsea110 <cutsea110@gmail.com>
2019-04-05 06:54:50 +0900

    [Refactor] uncurrying

--- src/Box100.hs
+++ src/Box100.hs
@@ -33,10 +33,10 @@ instance ApplicativeBifunctor TreeF where
 
 tip' :: a -> Cofree (TreeF a) a
 tip' n = Cf (In (Hisx (n, Tip n)))
-bin' :: Cofree (TreeF t) Integer -> Cofree (TreeF t) Integer -> Cofree (TreeF t) Integer
+bin' :: (Cofree (TreeF t) Integer, Cofree (TreeF t) Integer) -> Cofree (TreeF t) Integer
 bin' = binWith' (+)
-binWith' :: (a -> a -> a) -> Cofree (TreeF t) a -> Cofree (TreeF t) a -> Cofree (TreeF t) a
-binWith' op l r = Cf (In (Hisx (extract l `op` extract r, Bin (unCf l) (unCf r))))
+binWith' :: (a -> a -> a) -> (Cofree (TreeF t) a, Cofree (TreeF t) a) -> Cofree (TreeF t) a
+binWith' op (l, r) = Cf (In (Hisx (extract l `op` extract r, Bin (unCf l) (unCf r))))
 
 winder :: ((a, b) -> c) -> (b, [a]) -> Maybe (c, (c, [a]))
 winder f (y, xxs) = case xxs of
@@ -44,7 +44,7 @@ winder f (y, xxs) = case xxs of
   (x:xs) -> Just (y', (y', xs)) where y' = f (x, y)
 
 windCol :: (a -> a -> a) -> (Cofree (TreeF t) a, [Cofree (TreeF t) a]) -> [Cofree (TreeF t) a]
-windCol = unfoldr . winder . uncurry . binWith'
+windCol = unfoldr . winder . binWith'
 
 nexus :: (a -> a -> a) -> ([a], [a]) -> [[Cofree (TreeF a) a]]
 nexus op (cs, rs) = unfoldr psi (map tip' cs, map tip' rs)
