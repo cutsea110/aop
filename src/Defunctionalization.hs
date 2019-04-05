d33c777cffba43524c49027d1ecc09e62ccdab48
cutsea110 <cutsea110@gmail.com>
2019-04-05 12:03:37 +0900

    refine...

--- src/Defunctionalization.hs
+++ src/Defunctionalization.hs
@@ -3,7 +3,7 @@ module Defunctionalization where
 -- kazu_yamamoto throw a topic at haskell-jp slack.2019-04-05
 data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
 
-foldt (f, g) = u
+foldTree (f, g) = u
   where
     u (Leaf a) = f a
     u (Node l r) = g (u l) (u r)
@@ -11,23 +11,23 @@ foldt (f, g) = u
 cons x xs = x : xs
 o f g x = f (g x)
 flatten t = walk t []
-walk = foldt (cons, o)
+walk = foldTree (cons, o)
 
 ----------------------
 
 data Lam a = LamCons a | LamO (Lam a) (Lam a) deriving Show
 
-foldlam (f, g) = u
+foldLam (f, g) = u
   where
     u (LamCons a) = f a
     u (LamO l r) = g (u l) (u r)
 
-apply = foldlam (cons, o)
+apply = foldLam (cons, o)
 
 cons_def = LamCons
 o_def = LamO
 flatten_def t = apply (walk_def t) []
-walk_def = foldt (cons_def, o_def)
+walk_def = foldTree (cons_def, o_def)
 
 ----------------------
 
