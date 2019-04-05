a91486ab4473a71937cc5dac89822145b54b4f8d
cutsea110 <cutsea110@gmail.com>
2019-04-05 11:40:30 +0900

    commented.

--- src/Defunctionalization.hs
+++ src/Defunctionalization.hs
@@ -1,6 +1,6 @@
 module Defunctionalization where
 
--- kazu_yamamoto's question at haskell-jp slack.2019-04-05
+-- kazu_yamamoto throw a topic at haskell-jp slack.2019-04-05
 data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
 
 foldt (f, g) = u
@@ -28,3 +28,36 @@ cons_def = LamCons
 o_def = LamO
 flatten_def t = apply (walk_def t) []
 walk_def = foldt (cons_def, o_def)
+
+----------------------
+
+{--
+(1)  walk is a catamorphism on Tree.
+     walk is Tree's catamorphism of [(:),(.)]
+
+                  [Leaf, Node]
+             T  <------------------ A + T x T
+             |                         |
+   walk      |                         |
+             v    [(:),(.)]            v
+             X  <------------------ A + X x X
+
+
+(2) Lam is equivalent to Tree
+(3) walk_def is a Tree's catamorphism of [LamCons,LamO]
+    apply is a Lam's catamorphism of [(:),(.)]
+
+                  [Leaf, Node]
+             T  <------------------ A + T x T
+             |                         |
+  walk_def   |                         |
+             v    [LamCons,LamO]       v
+             L  <------------------ A + L x L
+             |                         |
+   apply     |                         |
+             v    [(:),(.)]            v
+             X  <------------------ A + X x X
+
+(4) walk is equivalent to apply . walk_def
+    These have a type of Tree a -> [a] -> [a]
+--}
