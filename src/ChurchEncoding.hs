08aa42aab0eccc921491024415dd9d4bf7b84db9
cutsea110 <cutsea110@gmail.com>
2019-04-06 10:37:30 +0900

    operator over nat

--- src/ChurchEncoding.hs
+++ src/ChurchEncoding.hs
@@ -11,6 +11,12 @@ pair (x1, x2) = \s -> s (x1, x2)
 first p = p (\(x1, x2) -> x1)
 second p = p (\(x1, x2) -> x2)
 
+-- ref.) https://en.wikipedia.org/wiki/Church_encoding
 zero f x = x
 one  f x = f x
 two f x = f (f x)
+
+suc n f x = f (n f x)
+plus m n f x = m f (n f x)
+mult m n f x = m (n f) x
+expr m n f x = (n m) f x
