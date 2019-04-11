10ae49d58001bae76fbef7f26201a97f0f755d7e
cutsea110 <cutsea110@gmail.com>
2019-04-11 09:48:22 +0900

    refer to https://github.com/Risto-Stevcev/haskell-church-encodings/blob/master/Rank1Types/Church.hs

--- src/ChurchEncoding.hs
+++ src/ChurchEncoding.hs
@@ -7,8 +7,8 @@ false a b = b
 true a b = a
 
 not p a b = p b a
-and p q = undefined -- p q p -- couldn't infer type
-or p q = undefined -- p p q -- couldn't infer type
+and p q = p q false -- p q p -- couldn't infer type
+or p q = p true q -- p p q -- couldn't infer type
 xor p q = p (not q) q
 
 testBool1 = bool true 1 2
