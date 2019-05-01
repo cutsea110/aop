e6edcbe695a0a487636b9d4cc352355561fea92a
cutsea110 <cutsea110@gmail.com>
2019-05-01 16:13:15 +0900

    add main

--- src/Bernoulli.hs
+++ src/Bernoulli.hs
@@ -87,3 +87,5 @@ bernoulli' = snd . histo phi
         (n, bs) = extract r
         ts' = map fromInteger [(n+2) `comb` k | k <- [0..n+1]]
         bn = (fromInteger(n+2)-sum(zipWith (*) ts' bs)) * recip(ts' !! fromInteger(n+1))
+
+main = mapM_ print (bernoulli' (toNat 50))
