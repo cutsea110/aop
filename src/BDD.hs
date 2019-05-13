db2391e5d6e03a42900d75d3ec14266c42deec83
cutsea110 <cutsea110@gmail.com>
2019-05-13 12:57:53 +0900

    impl

--- src/BDD.hs
+++ src/BDD.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances, TupleSections #-}
 module BDD where
 
+import Prelude hiding (either)
+
 import Control.Applicative (liftA2)
+import Data.Function (on)
 
 -- HaskellでSimpathを実装してみる
 -- by @yasuabe2613
@@ -15,3 +18,18 @@ edge = (liftA2 . liftA2) Edge min max
 
 start :: Edge
 start = edge 0 1
+
+onBoth :: (a -> a -> b) -> (Node -> a) -> Edge -> b
+onBoth f g (Edge l r) = (f `on` g) l r
+
+either :: (Node -> Bool) -> Edge -> Bool
+either = onBoth (||)
+
+contains :: Node -> Edge -> Bool
+contains = either . (==)
+
+modify :: (Node -> Node) -> Edge -> Edge
+modify = onBoth edge
+
+opposite :: Node -> Edge -> Node
+opposite n (Edge l r) = if l == n then r else l
