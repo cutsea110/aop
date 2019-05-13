cd9128270b8055f0e16326cada65752e5d1daa3e
cutsea110 <cutsea110@gmail.com>
2019-05-13 11:11:43 +0900

    ref.) https://qiita.com/yasuabe2613/items/f23b1d644d8aede87d97

--- src/BDD.hs
+++ src/BDD.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances, TupleSections #-}
 module BDD where
 
-data BDD = Top
-         | Bot
-         | Node { label   :: Integer
-                , branch0 :: BDD
-                , branch1 :: BDD
-                }
-         deriving (Show, Eq)
+import Control.Applicative (liftA2)
+
+-- HaskellでSimpathを実装してみる
+-- by @yasuabe2613
+-- https://qiita.com/yasuabe2613/items/f23b1d644d8aede87d97
+
+type Node = Int
+data Edge = Edge { left :: Node, right :: Node } deriving (Show, Eq, Ord)
+
+edge :: Node -> Node -> Edge
+edge = (liftA2 . liftA2) Edge min max
+
+start :: Edge
+start = edge 0 1
