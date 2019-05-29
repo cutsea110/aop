e1998f1c6f3f0fdd3d26a7f5741c00368baeaa9b
cutsea110 <cutsea110@gmail.com>
2019-05-30 06:51:39 +0900

    infinity of Tree

--- src/Inf.hs
+++ src/Inf.hs
@@ -4,14 +4,16 @@ import Prelude hiding (foldr)
 
 data Nat = Z | S Nat deriving (Show, Eq)
 
-foldn (c, f) = u
+foldn (c, f) = mu
   where
-    u Z = c
-    u (S n) = f (u n)
+    mu Z = c
+    mu (S n) = f (mu n)
 
-unfoldn psi x = case psi x of
-  Nothing -> Z
-  Just x' -> S (unfoldn psi x')
+unfoldn psi = nu
+  where
+    nu x = case psi x of
+      Nothing -> Z
+      Just x' -> S (nu x')
 
 idNat = foldn (Z, S)
 coIdNat = unfoldn psi
@@ -29,14 +31,16 @@ infNat = unfoldn (\x -> Just x) 0
 
 data List a = Nil | Cons a (List a) deriving (Show, Eq)
 
-foldr (c, f) = u
+foldr (c, f) = mu
   where
-    u Nil = c
-    u (Cons x xs) = f x (u xs)
+    mu Nil = c
+    mu (Cons x xs) = f x (mu xs)
 
-unfoldr psi x = case psi x of
-  Nothing -> Nil
-  Just (x, xs) -> Cons x (unfoldr psi xs)
+unfoldr psi = nu
+  where
+    nu x = case psi x of
+      Nothing -> Nil
+      Just (x, xs) -> Cons x (nu xs)
 
 
 idList = foldr (Nil, Cons)
@@ -54,11 +58,23 @@ infList = unfoldr (\x -> Just (x, x+1)) 0
 
 data Tree a = Leaf a | Node (Tree a) (Tree a) deriving (Show, Eq)
 
-foldt (f, g) = u
+foldt (f, g) = mu
   where
-    u (Leaf x) = f x
-    u (Node l r) = g (u l) (u r)
+    mu (Leaf x) = f x
+    mu (Node l r) = g (mu l) (mu r)
 
-unfoldt psi x = case psi x of
-  Left x -> Leaf x
-  Right (l, r) -> Node (unfoldt psi l) (unfoldt psi r)
+unfoldt psi = nu
+  where
+    nu x = case psi x of
+      Left x' -> Leaf x'
+      Right (l, r) -> Node (nu l) (nu r)
+
+idTree = foldt (Leaf, Node)
+coIdTree = unfoldt psi
+  where
+    psi (Leaf x) = Left x
+    psi (Node l r) = Right (l, r)
+
+infTree = unfoldt (\x -> Right (x, x+1)) 0
+
+infTree' = unfoldt (\x -> if x <= 0 then Left x else Right (x-1, x+1)) 1
