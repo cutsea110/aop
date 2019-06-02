8ca748134266b549b95b7deb6fd0d53d2ef1af9f
cutsea110 <cutsea110@gmail.com>
2019-06-02 14:46:57 +0900

    calculate combination of numbers which total equals to n

---
+++ src/CombPlus.hs
@@ -0,0 +1,15 @@
+module CombPlus where
+
+import Control.Monad
+import Data.List
+
+br 0 = [[],[0]]
+br n = nub $ concatMap (map sort . up) (br (n-1))
+
+up :: Num a => [a] -> [[a]]
+up [] = [[1]]
+up (n:ns) = (n+1:ns):map (n:) (up ns)
+
+pr xs = forM_ xs pr'
+  where
+    pr' = putStrLn . concat . intersperse " + " . map show
