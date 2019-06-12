d1c9dc036091c0bcaaf10336764b1f5bf934c65f
cutsea110 <cutsea110@gmail.com>
2019-06-13 08:58:23 +0900

    plus minus test

--- src/TutorialZygo.hs
+++ src/TutorialZygo.hs
@@ -2,3 +2,13 @@ module TutorialZygo where
 -- Ref.) https://stackoverflow.com/questions/36851766/histomorphisms-zygomorphisms-and-futumorphisms-specialised-to-lists
 --      Benjamin Hodgson's answer
 
+lengthEven :: [a] -> Bool
+lengthEven = even . length
+
+pm0 [] = 0
+pm0 (x:xs) = if lengthEven xs then x - pm0 xs else x + pm0 xs
+
+paraL f z [] = z
+paraL f z (x:xs) = f x xs (paraL f z xs)
+
+pm1 = paraL (\x xs acc -> if lengthEven xs then x - acc else x + acc) 0
