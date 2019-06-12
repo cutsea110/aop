56e83d88010efd8be22ede8bbe6bc06e2283c630
cutsea110 <cutsea110@gmail.com>
2019-06-13 08:52:24 +0900

    tutorial for zygo

---
+++ src/TutorialZygo.hs
@@ -0,0 +1,4 @@
+module TutorialZygo where
+-- Ref.) https://stackoverflow.com/questions/36851766/histomorphisms-zygomorphisms-and-futumorphisms-specialised-to-lists
+--      Benjamin Hodgson's answer
+
