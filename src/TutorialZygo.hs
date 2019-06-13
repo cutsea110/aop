bae2777f5fc5808b53be62b5b61791747bc22f5c
cutsea110 <cutsea110@gmail.com>
2019-06-13 09:14:45 +0900

    i'm got itgit add -u

--- src/TutorialZygo.hs
+++ src/TutorialZygo.hs
@@ -22,3 +22,11 @@ paraL' f z = snd . cataL (\x (xs, acc) -> (x:xs, f x xs acc)) ([], z)
 -- performance is good!
 -- for example, you check for [30000,29999..1] between pm1 to pm2
 pm2 = snd . cataL (\x (isEven, total) -> (not isEven, if isEven then x - total else x + total)) (True, 0)
+
+zygoL :: (a -> b -> b) -> -- a folding function
+         (a -> b -> c -> c) -> -- a folding function which depends on the result of the other fold
+         b -> c -> -- zeroes for the two folds
+         [a] -> c
+zygoL f g z e = snd . cataL (\x (p, q) -> (f x p, g x p q)) (z, e)
+
+pm3 = zygoL (\_ p -> not p) (\x isEven total -> if isEven then x - total else x + total) True 0
