fd84a8ec966372cabc91a2c26490397d00e8f842
cutsea110 <cutsea110@gmail.com>
2019-06-13 09:10:41 +0900

    pm2 is good performance

--- src/TutorialZygo.hs
+++ src/TutorialZygo.hs
@@ -12,3 +12,13 @@ paraL f z [] = z
 paraL f z (x:xs) = f x xs (paraL f z xs)
 
 pm1 = paraL (\x xs acc -> if lengthEven xs then x - acc else x + acc) 0
+
+cataL :: (a -> b -> b) -> b -> [a] -> b
+cataL = foldr
+
+lengthEven' = cataL (\_ p -> not p) True
+paraL' f z = snd . cataL (\x (xs, acc) -> (x:xs, f x xs acc)) ([], z)
+
+-- performance is good!
+-- for example, you check for [30000,29999..1] between pm1 to pm2
+pm2 = snd . cataL (\x (isEven, total) -> (not isEven, if isEven then x - total else x + total)) (True, 0)
