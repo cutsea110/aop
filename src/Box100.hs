8f75c41379825a9b44a59be7271bf0cff629cd20
cutsea110 <cutsea110@gmail.com>
2019-09-02 21:35:44 +0900

    nao's sample

--- src/Box100.hs
+++ src/Box100.hs
@@ -76,8 +76,11 @@ perfCheck act = do
   return (diffUTCTime e s)
 
 rows,cols :: [Int]
-rows = [4,2,5,6,7,1,3,9,3,2]
-cols = [8,2,4,6,1,8,9,3,1,7]
+-- rows = [4,2,5,6,7,1,3,9,3,2]
+-- cols = [8,2,4,6,1,8,9,3,1,7]
+-- nao's self problems
+rows = [8,6,2,7,9,20,12,6,10,9,5,4,1,0]
+cols = [0,1,20,3,10,6,7,9,8,2]
 
 main :: IO ()
 main = draw' 6 calc cols rows
