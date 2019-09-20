c64dc21358742b4e27aac3cc5ca0287ffd5e775f
cutsea110 <cutsea110@gmail.com>
2019-09-20 22:56:30 +0900

    refactor

--- src/Fix.hs
+++ src/Fix.hs
@@ -4,7 +4,7 @@
 #-}
 module Fix where
 
-import Prelude hiding (either, head, last)
+import Prelude hiding (either)
 
 pair (f, g) x = (f x, g x)
 cross (f, g) (x, y) = (f x, g y)
@@ -15,12 +15,12 @@ either (f, g) (Right x) = g x
 newtype Fix f = In { out :: f (Fix f) }
 
 newtype HisF f a = His { unHis :: (a, f (HisF f a)) }
-head :: HisF f t -> t
-head = fst . unHis
+extract :: HisF f t -> t
+extract = fst . unHis
 
 newtype FutF f a = Fut { unFut :: Either a (f (FutF f a)) }
-last :: a -> FutF f a
-last = Fut . Left
+inject :: a -> FutF f a
+inject = Fut . Left
 
 -- catamorphism
 cata :: Functor f => (f a -> a) -> Fix f -> a
@@ -42,10 +42,10 @@ apo :: Functor f => (t -> f (Either (Fix f) t)) -> t -> Fix f
 apo psi = In . fmap (either (id, apo psi)) . psi
 -- histomorphism
 histo :: Functor f => (f (HisF f t) -> t) -> Fix f -> t
-histo phi = head . cata (His . pair (phi, id))
+histo phi = extract . cata (His . pair (phi, id))
 -- futumorphism
 futu :: Functor f => (t -> f (FutF f t)) -> t -> Fix f
-futu psi = ana (either (psi, id) . unFut) . last
+futu psi = ana (either (psi, id) . unFut) . inject
 -- chronomorphism
 chrono :: Functor f => (f (HisF f b) -> b) -> (a -> f (FutF f a)) -> a -> b
 chrono phi psi = histo phi . futu psi
@@ -57,10 +57,10 @@ cozygo :: Functor f => (a -> f a) -> (b -> f (Either a b)) -> b -> Fix f
 cozygo f psi = ana (either (fmap Left . f, psi)) . Right
 -- dynamorphism
 dyna :: Functor f => (f (HisF f b) -> b) -> (a -> f a) -> a -> b
-dyna f g = chrono f (fmap last . g)
+dyna f g = chrono f (fmap inject . g)
 -- codynamorphism
 codyna :: Functor f => (f b -> b) -> (a -> f (FutF f a)) -> a -> b
-codyna f g = chrono (f . fmap head) g
+codyna f g = chrono (f . fmap extract) g
 -- mutumorphism
 mutu :: Functor f => (a -> b) -> (f a -> a) -> Fix f -> b
 mutu proj phi = proj . cata phi
