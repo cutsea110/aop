0fe984c8d44c687edb7b988b2664ba766613a0b9
cutsea110 <cutsea110@gmail.com>
2019-09-20 23:11:03 +0900

    add cochrono

--- src/Fix.hs
+++ src/Fix.hs
@@ -4,23 +4,18 @@
 #-}
 module Fix where
 
-import Prelude hiding (either)
-
 pair (f, g) x = (f x, g x)
 cross (f, g) (x, y) = (f x, g y)
-either (f, g) (Left x) = f x
-either (f, g) (Right x) = g x
-
 
 newtype Fix f = In { out :: f (Fix f) }
 
-newtype HisF f a = His { unHis :: (a, f (HisF f a)) }
-extract :: HisF f t -> t
-extract = fst . unHis
+newtype Cofree f a = Cf { unCf :: (a, f (Cofree f a)) }
+extract :: Cofree f t -> t
+extract = fst . unCf
 
-newtype FutF f a = Fut { unFut :: Either a (f (FutF f a)) }
-inject :: a -> FutF f a
-inject = Fut . Left
+newtype Free f a = Fr { unFr :: Either a (f (Free f a)) }
+inject :: a -> Free f a
+inject = Fr . Left
 
 -- catamorphism
 cata :: Functor f => (f a -> a) -> Fix f -> a
@@ -39,27 +34,30 @@ para :: Functor f => (f (Fix f, t) -> t) -> Fix f -> t
 para phi = phi . fmap (pair (id, para phi)) . out
 -- apomorphism
 apo :: Functor f => (t -> f (Either (Fix f) t)) -> t -> Fix f
-apo psi = In . fmap (either (id, apo psi)) . psi
+apo psi = In . fmap (uncurry either (id, apo psi)) . psi
 -- histomorphism
-histo :: Functor f => (f (HisF f t) -> t) -> Fix f -> t
-histo phi = extract . cata (His . pair (phi, id))
+histo :: Functor f => (f (Cofree f t) -> t) -> Fix f -> t
+histo phi = extract . cata (Cf . pair (phi, id))
 -- futumorphism
-futu :: Functor f => (t -> f (FutF f t)) -> t -> Fix f
-futu psi = ana (either (psi, id) . unFut) . inject
+futu :: Functor f => (t -> f (Free f t)) -> t -> Fix f
+futu psi = ana (uncurry either (psi, id) . unFr) . inject
 -- chronomorphism
-chrono :: Functor f => (f (HisF f b) -> b) -> (a -> f (FutF f a)) -> a -> b
+chrono :: Functor f => (f (Cofree f b) -> b) -> (a -> f (Free f a)) -> a -> b
 chrono phi psi = histo phi . futu psi
+-- cochronomorphism
+cochrono :: Functor f => (f (Cofree f t) -> t) -> (t -> f (Free f t)) -> Fix f -> Fix f
+cochrono phi psi = futu psi . histo phi
 -- zygomorphism
 zygo :: Functor f => (f a -> a) -> (f (a, b) -> b) -> Fix f -> b
 zygo f phi = snd . cata (pair (f . fmap fst, phi))
 -- cozygomorphism
 cozygo :: Functor f => (a -> f a) -> (b -> f (Either a b)) -> b -> Fix f
-cozygo f psi = ana (either (fmap Left . f, psi)) . Right
+cozygo f psi = ana (uncurry either (fmap Left . f, psi)) . Right
 -- dynamorphism
-dyna :: Functor f => (f (HisF f b) -> b) -> (a -> f a) -> a -> b
+dyna :: Functor f => (f (Cofree f b) -> b) -> (a -> f a) -> a -> b
 dyna f g = chrono f (fmap inject . g)
 -- codynamorphism
-codyna :: Functor f => (f b -> b) -> (a -> f (FutF f a)) -> a -> b
+codyna :: Functor f => (f b -> b) -> (a -> f (Free f a)) -> a -> b
 codyna f g = chrono (f . fmap extract) g
 -- mutumorphism
 mutu :: Functor f => (a -> b) -> (f a -> a) -> Fix f -> b
