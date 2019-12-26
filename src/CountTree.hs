0bba5887dc66b332b81bba0fb88837cf64a66394
cutsea110 <cutsea110@gmail.com>
2019-12-26 14:07:49 +0900

    update

--- src/CountTree.hs
+++ src/CountTree.hs
@@ -12,11 +12,11 @@ import Data.Functor.Foldable
 
 data Tree = Leaf
           | Tree :^: Tree
-          deriving Eq
+          deriving (Eq, Show)
 
 data TreeF r = LeafF
              | r :^^: r
-             deriving Eq
+             deriving (Eq, Show)
 
 instance Functor TreeF where
   fmap f = \case
@@ -34,3 +34,28 @@ instance Corecursive Tree where
   embed = \case
     LeafF -> Leaf
     l :^^: r -> l :^: r
+
+trees :: Natural -> [Tree]
+trees = \case
+  0   -> [Leaf]
+  n+1 -> [s :^: t | (l, r) <- splits n, s <- trees l, t <- trees r]
+
+splits :: Natural -> [(Natural, Natural)]
+splits = para phi
+  where
+    phi = \case
+      Nothing -> [(0, 0)]
+      Just (n, ds) -> (0, n) : map (first succ) ds
+
+allTrees :: [Tree]
+allTrees = concatMap trees [0..]
+
+toIndex :: Tree -> Natural
+toIndex = head . flip genericFindIndices allTrees . (==)
+  where
+    genericFindIndices :: (Integral n) => (a -> Bool) -> [a] -> [n]
+    genericFindIndices p xs = [ i | (x, i) <- zip xs [0..], p x ]
+
+fromIndex :: Natural -> Tree
+fromIndex = genericIndex allTrees
+
