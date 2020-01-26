010956300f335c9b7414252b73c0c22b06280714
cutsea110 <cutsea110@gmail.com>
2020-01-27 07:38:03 +0900

    define Category

--- src/CategoryTheory.hs
+++ src/CategoryTheory.hs
@@ -1,1 +1,16 @@
+{-# LANGUAGE InstanceSigs #-}
 module CategoryTheory where
+
+import Prelude ()
+
+class Category cat where
+  id :: cat a a
+  (.) :: cat b c -> cat a b -> cat a c
+
+instance Category (->) where
+  id :: (->) a a
+  id a = a
+
+  (.) :: (->) b c -> (->) a b -> (->) a c
+  f . g = \x -> f (g x)
+
