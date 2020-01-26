1cf2e2bf528ddf36ec6d124c5e2b4e95dc49f34f
cutsea110 <cutsea110@gmail.com>
2020-01-27 08:09:33 +0900

    Lens as a example of Category

--- src/CategoryTheory.hs
+++ src/CategoryTheory.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE InstanceSigs #-}
 module CategoryTheory where
 
-import Prelude hiding (id, (.))
-
 class Category cat where
   id :: cat a a
   (.) :: cat b c -> cat a b -> cat a c
@@ -22,3 +20,14 @@ instance Monad m => Category (Kleisli m) where
 
   (.) :: (Kleisli m) b c -> (Kleisli m) a b -> (Kleisli m) a c
   f . g = Kleisli (\x -> runKleisli g x >>= runKleisli f)
+
+data Lens a b = Lens (a -> b) (a -> b -> a)
+
+instance Category Lens where
+  id :: Lens a a
+  id = Lens Prelude.id (const Prelude.id)
+
+  (.) :: Lens b c -> Lens a b -> Lens a c
+  Lens g1 s1 . Lens g2 s2 = Lens (g1 Prelude.. g2) s3
+    where
+      s3 a c = s2 a (s1 (g2 a) c)
