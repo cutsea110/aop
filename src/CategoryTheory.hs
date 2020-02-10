f3517e15c33b1234c22074eb6cd738f69dadc890
cutsea110 <cutsea110@gmail.com>
2020-02-11 08:15:02 +0900

    define class Comonad

--- src/CategoryTheory.hs
+++ src/CategoryTheory.hs
@@ -174,3 +174,11 @@ instance Monad' ((,) s) ((->) s) where
   join' :: (s -> (s, (s -> (s, a)))) -> (s -> (s, a))
   join' f s = f' s' where (s', f') = f s
 
+class Functor w => Comonad w where
+  extract :: w a -> a
+
+  duplicate :: w a -> w (w a)
+  duplicate = extend id'
+
+  extend :: (w a -> b) -> w a -> w b
+  extend f = fmap f * duplicate
