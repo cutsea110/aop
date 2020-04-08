0517153219897a9568949e337717760e5b06b0a1
cutsea110 <cutsea110@gmail.com>
2020-04-08 09:18:30 +0900

    add comment

--- src/Subfactorial.hs
+++ src/Subfactorial.hs
@@ -14,12 +14,18 @@ paran (c, f) = u
   where u 0 = c
         u (n+1) = f (n+1) (u n)
 
+-- >>> fact 9
+-- 362880
 fact = paran (1, (*))
 
+-- >>> subfact 9
+-- 133496
 subfact 1 = 0
 subfact 2 = 1
 subfact n = snd $ paran ((0, 1), \i (x, y) -> (y, (i-1)*(x+y))) n
 
+-- >>> e 9
+-- 2.71828369389345
 -- >>> e 20
 -- 2.718281828459045
 -- >>> exp 1
