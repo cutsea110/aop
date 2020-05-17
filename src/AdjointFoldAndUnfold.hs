3b008854616eb8d6dea8e13b0e12b982392c3770
cutsea110 <cutsea110@gmail.com>
2020-05-18 08:57:05 +0900

    commented 'Mendler-style'

--- src/AdjointFoldAndUnfold.hs
+++ src/AdjointFoldAndUnfold.hs
@@ -16,12 +16,16 @@ total :: Fix Stack -> Natural
 total (In Empty) = 0
 total (In (Push (n, s))) = n + total s
 
+-- | Mendler-style
+-- psi == total'
+-- x   == ttl
+-- a   == Empty and Pust (n, s)
 total' :: (t -> Natural) -> Stack t -> Natural
 total' ttl Empty = 0
 total' ttl (Push (n, s)) = n + ttl s
 
 ttl :: Fix Stack -> Natural
-ttl (In s) = total' ttl s
+ttl (In s) = total' ttl s    -- Mendler-style equation : x (In a) == psi x a
 
 data Sequ s = Next (Natural, s) deriving Show
 instance Functor Sequ where
@@ -30,8 +34,12 @@ instance Functor Sequ where
 from :: Natural -> Cofix Sequ
 from n = UnOut (Next (n, from (n+1)))
 
+-- | Mendler-style
+-- psi == from'
+-- x   == frm
+-- a   == n
 from' :: (Natural -> s) -> Natural -> Sequ s
 from' frm n = Next (n, frm (n+1))
 
 frm :: Natural -> Cofix Sequ
-frm n = UnOut (from' frm n)
+frm n = UnOut (from' frm n)  -- Mendler-style equation : x a = UnOut (psi x a)
