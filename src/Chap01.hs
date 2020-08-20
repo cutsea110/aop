364a1b2be49bac224c74a60eb9148b715ded74a0
cutsea110 <cutsea110@gmail.com>
2020-08-20 09:03:39 +0900

    add solution 2 for 1.1

--- src/Chap01.hs
+++ src/Chap01.hs
@@ -5,10 +5,13 @@ module Chap01 where
 k1 x = if k1 x == 0 then 1 else 0
 
 -- solution 2
-k2 x = k2 x + 1
+k2 x = not (k2 x)
 
 -- solution 3
-k3 x = k3 x ++ [x]
+k3 x = k3 x + 1
+
+-- solution 4
+k4 x = k4 x ++ [x]
 
 -- | Ex 1.2
 m (x, y) = if x == y
