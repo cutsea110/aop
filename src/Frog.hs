d434db90594ea066e752bb47c3fb516f92a32360
cutsea110 <cutsea110@gmail.com>
2020-11-07 12:48:32 +0900

    refine

--- src/Frog.hs
+++ src/Frog.hs
@@ -1,21 +1,24 @@
 module Frog where
 
+import Data.Char (isSpace)
+import qualified Data.ByteString.Char8 as C
 import qualified Data.Vector as V
 
-hs_list = [10,40,30,20]
-hs = V.fromList hs_list
-idx = [1..n]
-  where n = length hs_list - 1
+parseInt = C.readInt . C.dropWhile isSpace
+getIntVec n = V.unfoldrN n parseInt <$> C.getLine
 
-naive_f :: Int -> Int
-naive_f 0 = 0
-naive_f 1 = abs $ hs V.! 1 - hs V.! 0
-naive_f n = min (naive_f (n-1) + abs (hs V.! n - hs V.! (n-1))) (naive_f (n-2) + abs (hs V.! n - hs V.! (n-2)))
+n = 4
+hs = V.fromList [10,40,30,20]
 
 f mf 0 = 0
 f mf 1 = abs $ hs V.! 1 - hs V.! 0
 f mf n = min (mf (n-1) + abs (hs V.! n - hs V.! (n-1))) (mf (n-2) + abs (hs V.! n - hs V.! (n-2)))
 
-f_vector = V.map (f faster_f) $ V.fromList [0..3]
+f_vector = V.map (f faster_f) $ V.fromList [0..(n-1)]
 
 faster_f = (f_vector V.!)
+
+main = do
+  n <- readLn :: IO Int
+  hs <- getIntVec n
+  print $ faster_f (n-1)
