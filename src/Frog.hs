00a5687b0f2ff7b9ef9dfbc33fbf3c063754f3c7
cutsea110 <cutsea110@gmail.com>
2020-11-07 15:58:47 +0900

    retrying

--- src/Frog.hs
+++ src/Frog.hs
@@ -1,20 +1,24 @@
 module Frog where
 
-import Data.Array
-import qualified Data.ByteString.Char8 as C
 import Data.Char (isSpace)
+import qualified Data.ByteString.Char8 as C
+import qualified Data.Vector as V
 
-frog :: Int -> [Integer] -> Array Int Integer
-frog n xs = arr where
-  hs = listArray (1, n) xs
-  idx = listArray (1, n) [1..n]
-  arr = fmap (f faster_f) idx
-    where faster_f = (arr !)
-          f mf 1 = 0
-          f mf 2 = abs $ hs!2 - hs!1
-          f mf n = min (mf (n-1) + abs(hs!n - hs!(n-1))) (mf (n-2) + abs(hs!n - hs!(n-2)))
+parseInt = C.readInt . C.dropWhile isSpace
+getIntVec n = V.unfoldrN n parseInt <$> C.getLine
+
+n = 6
+hs_list = [30,10,60,10,60,50] -- [10,40,30,20]
+
+f_vector = V.map (f faster_f) $ V.fromList [0..(n-1)]
+  where
+    hs = V.fromList hs_list
+    f mf 0 = 0
+    f mf 1 = abs $ hs V.! 1 - hs V.! 0
+    f mf i = min (mf (i-1) + abs (hs V.! i - hs V.! (i-1))) (mf (i-2) + abs (hs V.! i - hs V.! (i-2)))
+    faster_f = (f_vector V.!)
 
 main = do
   n <- readLn :: IO Int
-  xs <- fmap read . words <$> getLine
-  print $ frog n xs ! n
+  hs <- getIntVec n
+  print $ f_vector V.! (n-1)
