74d2bc8125d3c6849076709c16fafe877987f178
cutsea110 <cutsea110@gmail.com>
2020-12-16 10:30:06 +0900

    fix subseq to subseteq

--- src/Chap04.hs
+++ src/Chap04.hs
@@ -2,14 +2,15 @@ module Chap04 where
 
 -- | Ex 4.1
 --
---  X \subseq R \cap (S \cap T)
+--  X \subseteq R \cap (S \cap T)
 -- = {- 交わりの普遍性 -}
---  X \subseq R /\ X \subseq (S \cap T)
+--  X \subseteq R /\ X \subseteq (S \cap T)
 -- = {- 交わりの普遍性 -}
---  X \subseq R /\ (X \subseq S /\ X \subseq T)
+--  X \subseteq R /\ (X \subseteq S /\ X \subseteq T)
 -- = {- 連言は結合的 -}
---  (X \subseq R /\ X \subseq S) /\ X \subseq T
+--  (X \subseteq R /\ X \subseteq S) /\ X \subseteq T
 -- = {- 交わりの普遍性 -}
---  X \subseq (R \cap S) /\ X \subseq T
+--  X \subseteq (R \cap S) /\ X \subseteq T
 -- = {- 交わりの普遍性 -}
---  X \subseq (R \cap S) \cap T
+--  X \subseteq (R \cap S) \cap T
+
