3c14abe5055ef649b4dadc7e046a7f6842d302f4
cutsea110 <cutsea110@gmail.com>
2021-02-07 09:59:01 +0900

    ugry?

--- src/ZipUnzip.hs
+++ src/ZipUnzip.hs
@@ -5,11 +5,23 @@ module ZipUnzip where
 import Prelude hiding (zip, unzip)
 import Data.List (unfoldr)
 
+foldr' :: (b, (a, b) -> b) -> [a] -> b
+foldr' phi@(c, f) = u
+  where u [] = c
+        u (x:xs) = f (x, u xs)
 
+unzip :: [(a, b)] -> ([a], [b])
+unzip = foldr' phi
+  where phi = (nils, conss)
+        nils = ([], [])
+        conss ((a, b), (xs, ys)) = (a:xs, b:ys)
+
+{-
 unzip :: [(a, b)] -> ([a], [b])
 unzip = foldr conss nils
   where nils = ([], [])
         conss (a, b) (xs, ys) = (a:xs, b:ys)
+-}
 
 zip :: ([a], [b]) -> [(a, b)]
 zip = unfoldr psi
