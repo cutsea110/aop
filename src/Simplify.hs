686ab6ca6f19897edd30fbc809fe266622a28ed8
cutsea110 <cutsea110@gmail.com>
2021-05-23 16:08:53 +0900

    test3

--- src/Simplify.hs
+++ src/Simplify.hs
@@ -38,4 +38,34 @@ test = prove customLaws "filter (all nodups . boxs) . expand . pruneBy boxs = fi
                  , "expand-boxs則: expand . boxs = map boxs . expand"
                  , "hack: map boxs . cp . map cp = cp . map cp . boxs"
                  ]
-    
+
+-- TFWH p312 12.8 運算器のテスト
+test2 :: Calculation
+test2 = prove customLaws "xmatch = cmap xmatchesA . cpp . (one * alignments)"
+  where
+    customLaws :: [String]
+    customLaws = [ "matchの定義: match = cmap matchesA . alignments"
+                 , "xmatchの定義: xmatch = cup . (one * match)"
+                 , "xmatchesAの定義: xmatchesA = cup . (one * matchesA)"
+                 , "(*)の双ファンクタ則: (f * g) . (h * k) = (f . h) * (g . k)"
+                 , "cmap-cup則: cmap (cup . (one * g)) . cpp = cup . (id * cmap g)"
+                 ]
+
+test3 :: Calculation
+test3 = prove customLaws "xmatch s = cmap (xmatchesA s) . alignments"
+  where
+    customLaws = [ "matchの定義: match = cmap matchesA . alignments"
+                 , "xmatchの定義: xmatch s = cmap (unify s) . match"
+                 , "xmatchesAの定義: xmatchesA s = cmap (unify s) . matchesA"
+                 , "cmap-cmap則: cmap f . cmap g = cmap (cmap f . g)"
+                 ]
+
+testX :: Calculation
+testX = prove customLaws "xmatchesA . (id * nil) = one . fst"
+  where
+    customLaws :: [String]
+    customLaws = [ "matchの定義: match = cmap matchesA . alignments"
+                 , "matchesAの定義: matchesA = combine . map matchA"
+                 , "xmatchの定義: xmatch = cup . (one * match)"
+                 , "xmatchesAの定義: "
+                 ]
