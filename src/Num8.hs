7e8449e1c59e32da293004ce12b8f044eda41c5f
cutsea110 <cutsea110@gmail.com>
2021-06-01 19:57:21 +0900

    refine

--- src/Num8.hs
+++ src/Num8.hs
@@ -10,15 +10,16 @@ select :: [Int] -> [(Int, [Int])]
 select [x] = [(x, [])]
 select (x:xs) = (x,xs) : map (\(y, ys) -> (y, x:ys)) (select xs)
 
-perms :: Int -> [Int] -> [[Int]]
-perms 0 _ = [[]]
-perms n xs = [y:zs | (y, ys) <- select xs, zs <- perms (n-1) ys]
+perms :: Int -> [Int] -> [([Int], [Int])]
+perms 0 [] = [([], [])]
+perms 0 xs = [([], xs)]
+perms n xs = [(y:zs, ws) | (y, ys) <- select xs, (zs, ws) <- perms (n-1) ys]
 
 gen :: Int -> [([Int], [Int])]
-gen n = [ (xs, ys) | xs <- perms n [0..9], ys <- perms n ([0..9] \\ xs)]
+gen n = [(xs, zs) | (xs, ys) <- perms n [0..9], (zs, ws) <- perms n ys, xs < zs]
 
 calc :: [Int] -> [Int] -> (Int, Int, Int)
-calc xs ys = (xs', ys', abs(xs'-ys'))
+calc xs ys = (xs', ys', ys'-xs')
   where
     num i j = i*10+j
     tapply f (x, y) = (f x, f y)
@@ -30,7 +31,8 @@ num8 = forM_ (gen 4) $ \(xs, ys) -> do
   (_, _, v) <- get
   when (v' < v) $ do
     { put upd
-    ; liftIO $ print upd
+    ; liftIO $ putStrLn $ "=> " ++ show upd
     }
 
+main :: IO (Int, Int, Int)
 main = evalStateT (num8 >> get) (9999, 0, 9999)
