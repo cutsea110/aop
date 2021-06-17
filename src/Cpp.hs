630155fc40b8a7e9a22ba2beae6b6b8037116b73
cutsea110 <cutsea110@gmail.com>
2021-06-18 07:46:08 +0900

    make monadic

--- src/Cpp.hs
+++ src/Cpp.hs
@@ -5,3 +5,16 @@ import FixPrime
 cpp (x, y) = [(a, b) | a <- x, b <- y]
 cpr (a, y) = [(a, b) | b <- y]
 cpl (x, b) = [(a, b) | a <- x]
+
+cpp' (x, y) = do
+  a <- x
+  b <- y
+  return (a, b)
+
+cpr' (a, y) = do
+  b <- y
+  return (a, b)
+
+cpl' (x, b) = do
+  a <- x
+  return (a, b)
