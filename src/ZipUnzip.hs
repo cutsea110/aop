94470d3fd6bdfc2f8fafb2887eaa8a214e38938f
cutsea110 <cutsea110@gmail.com>
2021-07-13 05:18:14 +0900

    define unfoldr

--- src/ZipUnzip.hs
+++ src/ZipUnzip.hs
@@ -2,16 +2,22 @@
 module ZipUnzip where
 
 -- 逆により仕様
-import Prelude hiding (zip, unzip)
-import Data.List (unfoldr)
+import Prelude hiding (zip, unzip, foldr)
+-- import Data.List (unfoldr)
 
-foldr' :: (b, (a, b) -> b) -> [a] -> b
-foldr' phi@(c, f) = u
+foldr :: (b, (a, b) -> b) -> [a] -> b
+foldr phi@(c, f) = u
   where u [] = c
         u (x:xs) = f (x, u xs)
 
+unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+unfoldr psi = v
+  where v b = case psi b of
+          Nothing      -> []
+          Just (a, b') -> a:v b'
+
 unzip :: [(a, b)] -> ([a], [b])
-unzip = foldr' phi
+unzip = foldr phi
   where phi = (nils, conss)
         nils = ([], [])
         conss ((a, b), (xs, ys)) = (a:xs, b:ys)
@@ -25,9 +31,10 @@ unzip = foldr conss nils
 
 zip :: ([a], [b]) -> [(a, b)]
 zip = unfoldr psi
-  where psi ([], []) = Nothing
-        psi (a:xs, b:ys) = Just ((a, b), (xs, ys))
-
+  where psi (a:xs, b:ys) = Just ((a, b), (xs, ys))
+     -- psi ([], [])     = Nothing                 -- more strict
+        psi _            = Nothing                 -- lax definition
+        
 unzip' [] = ([], [])
 unzip' ((a,b):xys) = (a:xs, b:ys) where (xs, ys) = unzip' xys
 
