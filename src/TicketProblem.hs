9b5cd74d35e61db2eb92f4c6190905f8dd932af3
cutsea110 <cutsea110@gmail.com>
2021-07-14 15:02:51 +0900

    implement ticket

--- src/TicketProblem.hs
+++ src/TicketProblem.hs
@@ -1,36 +1,9 @@
 -- | ref.) https://www.ipsj.or.jp/07editj/promenade/4605.pdf
 module TicketProblem where
-{--
-data DumbTree = Empty | Fork DumbTree DumbTree
-instance Show DumbTree where
-  show Empty = "0"
-  show (Fork Empty Empty) = "(" ++ "L" ++ "^" ++ "R" ++ ")"
-  show (Fork Empty r) = "(" ++ "L" ++ "^" ++ show r ++ ")"
-  show (Fork l Empty) = "(" ++ show l ++ "^" ++ "R" ++ ")"
-  show (Fork l r) = "(" ++ show l ++ "^" ++ show r ++ ")"
 
--- trees 整数(>0)から, DumbTree のリストへ
-trees :: Int -> [DumbTree]
-trees 1 = [Empty]
-trees n = concat [ joins ls rs
-                 | (ls, rs) <- [ lrs xs ys
-                               | (xs, ys) <- splits1 n
-                               ]
-                 ]
+import Data.Char (ord)
 
-splits1 :: Int -> [(Int, Int)]
-splits1 1 = []
-splits1 n = (1, n-1):[ (i+1, j)
-                     | (i, j) <- splits1 (n-1)
-                     ]
-
-lrs :: Int -> Int -> ([DumbTree], [DumbTree])
-lrs xs ys = (trees xs, trees ys)
-
-joins :: [DumbTree] -> [DumbTree] -> [DumbTree]
-joins ls rs = [ Fork l r | l <- ls, r <- rs ]
---}
-
+type Rat = (Int, Int)
 data Term = Val Char | App Char Term Term
 
 trees :: [Char] -> [Char] -> [Term]
@@ -59,3 +32,42 @@ otree os (App _ l r) = (os'', App o' l' r')
 instance Show Term where
   show (Val c) = [c]
   show (App o l r) = "(" ++ show l ++ [o] ++ show r ++ ")"
+
+eval :: Term -> Rat
+eval (Val x)     = ctor x
+eval (App o l r) = (ctoo o) (eval l) (eval r)
+
+ctor :: Char -> Rat
+ctor x = (ord x - ord '0', 1)
+
+ctoo :: Char -> (Rat -> Rat -> Rat)
+ctoo '+' (x, y) (z, w) = (x*w+z*y, y*w)
+ctoo '-' (x, y) (z, w) = (x*w-z*y, y*w)
+ctoo '*' (x, y) (z, w) = (x*z, y*w)
+ctoo '/' (x, y) (z, w) = if z == 0 then (0, 0) else (x*w, y*z)
+
+ticket :: Int -> [Char] -> Term
+ticket n ds = head (filter (same n) (allterms ds))
+
+same :: Int -> (Term -> Bool)
+same i t = i*d == n && d /= 0
+  where (n, d) = eval t
+
+allterms :: [Char] -> [Term]
+allterms ds = concat [ trees ns os | ns <- perm ds, os <- rperm ops4 (length ds - 1) ]
+  where pm _ [] = []
+        pm hs (t:ts) = [ t:ys | ys <- perm (hs ++ ts) ]
+
+ops4 = "+-*/"
+
+perm [] = [[]]
+perm xs = concat [ pm hs ts | (hs, ts) <- splits xs ]
+  where pm _  []     = []
+        pm hs (t:ts) = [ t:ys | ys <- perm (hs ++ ts) ]
+
+splits [] = [([], [])]
+splits (x:xs) = ([], x:xs) : [ (x:ys, zs) | (ys, zs) <- splits xs ]
+
+rperm _  0 = [[]]
+rperm [] _ = []
+rperm xs n = [ x:ys | x <- xs, ys <- rperm xs (n-1) ]
