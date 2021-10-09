532954beef32dbc35f57861dff939fdd8f035bc4
cutsea110 <cutsea110@gmail.com>
2021-10-09 20:44:09 +0900

    inc' use a mutable Vector

--- src/BinaryIndexedTree.hs
+++ src/BinaryIndexedTree.hs
@@ -1,8 +1,11 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, RankNTypes, GADTs #-}
 module BinaryIndexedTree where
 
 import Prelude hiding (sum)
 
+import Control.Monad (forM_)
+import Control.Monad.ST
+import Control.Monad.Primitive (PrimState, PrimMonad)
 import Data.Bits
 import Data.Monoid
 import qualified Data.Vector.Mutable as M
@@ -25,12 +28,28 @@ new n = BIT n (V.replicate (n+1) mempty) -- we can use from 1-index.
 
 {- |
 increment the value at index i(1-base) by amount w. O(log n)
+But this use immutable Vector.
 -}
 inc :: Monoid a => Int -> a -> BIT a -> BIT a
 inc i w b | 1 <= i && i <= n = b { vec = V.accum (<>) v $ zip xs (repeat w) }
           | otherwise = error $ "index must be between 1 to " ++ show n
   where (n, v) = (getN b, vec b)
         xs = takeWhile (<=n) $ iterate (\x -> x + x .&. (-x)) i
+
+{- |
+increment the value at index i(1-base) by amount w. O(log n)
+this use mutable Vector.
+-}
+inc' :: Monoid a => Int -> a -> BIT a -> BIT a
+inc' i w b =
+  BIT n $ V.create $ do
+    mv <- V.thaw v
+    forM_ xs $ \i -> M.modify mv (<>w) i
+    return mv
+  where
+    (n, v) = (getN b, vec b)
+    xs = takeWhile (<=n) $ iterate (\x -> x + x .&. (-x)) i
+
 
 {- |
 lookup the sum of all values from index 1 to index i. O(log n)
@@ -40,7 +59,6 @@ b ! i | 1 <= i && i <= n = foldr (\i acc -> acc <> (v V.! i)) mempty xs
       | otherwise = error $ "index must be between 1 to " ++ show n
   where (n, v) = (getN b, vec b)
         xs = takeWhile (>0) $ iterate (\x -> x - x .&. (-x)) i
-
 
 --------------------------------------------------------------------------------
 
@@ -57,7 +75,7 @@ inverse9 xs = map (second getSum) $ go xs b []
         b = new n :: BIT (Sum Int)
         go []     b acc = zip xs (reverse acc)
         go (x:xs) b acc = let acc' = b ! n - b ! x : acc
-                              b' = inc x 1 b
+                              b' = inc' x 1 b
                           in go xs b' acc'
 
 {- | 転倒対象リスト
@@ -72,7 +90,7 @@ inverse9' xs = go xs b []
         b = new n :: BIT [Int]
         go []     b acc = zip xs (reverse acc)
         go (x:xs) b acc = let acc' = (b ! n \\ b ! x) : acc
-                              b' = inc x [x] b
+                              b' = inc' x [x] b
                           in go xs b' acc'
 
 {- | 転倒対象インデックスリスト
@@ -87,5 +105,5 @@ inverse9'i xs = go (zip [0..] xs) b []
         b = new n :: BIT [Int]
         go []             b acc = zip xs (reverse acc)
         go (ix@(i, x):xs) b acc = let acc' = (b ! n \\ b ! x) : acc
-                                      b' = inc x [i] b
+                                      b' = inc' x [i] b
                                   in go xs b' acc'
