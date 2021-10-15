790ecbe12314ee5e2fb112130c32ae32fd2a4424
cutsea110 <cutsea110@gmail.com>
2021-10-15 11:35:40 +0900

    Ralf Hize's sorting.pdf

---
+++ src/SortOverMorphism.hs
@@ -0,0 +1,19 @@
+module SortOverMorphism where
+-- ref.) http://www.cs.ox.ac.uk/people/daniel.james/sorting/sorting.pdf
+import Data.List (partition, unfoldr, delete)
+
+insertSort :: [Integer] -> [Integer]
+insertSort = foldr insert []
+
+insert :: Integer -> [Integer] -> [Integer]
+insert y ys = xs ++ [y] ++ zs
+  where (xs, zs) = id $? partition (<y) ys
+  
+selectSort :: [Integer] -> [Integer]
+selectSort = unfoldr select
+
+select :: [Integer] -> Maybe (Integer, [Integer])
+select [] = Nothing
+select xs = Just (x, xs')
+  where x = minimum xs
+        xs' = delete x xs
