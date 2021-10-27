727550875558752b442db1f5c2be91c9fef97d9d
cutsea110 <cutsea110@gmail.com>
2021-10-27 18:31:50 +0900

    trying...

--- src/StudyICBICSort.hs
+++ src/StudyICBICSort.hs
@@ -1,39 +1,33 @@
-{-# LANGUAGE TemplateHaskell, TypeFamilies, KindSignatures #-}
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# LANGUAGE DeriveFunctor #-}
 module StudyICBICSort where
 
 import Debug.Trace (trace)
 
-import Data.Functor.Foldable
-import Data.Functor.Foldable.TH
+import Fix
 
 debug = True
 
 ($?) :: Show a => (a -> b) -> a -> b
 f $? x = if debug then trace (show x) (f x) else f x
 
--- | dual for either (euclidian too)
-pair :: (c -> a) -> (c -> b) -> c -> (a, b)
-pair f g x = (f x, g x)
 
 -- | recursion structure for Euclid's algorithm
 data Euclidian a = Triv a | Same (Euclidian a) deriving Show
-makeBaseFunctor ''Euclidian
+
+data EuclidianF a r = TrivF a | SameF r deriving (Show, Functor)
+
 -- | isomorphic to either, EuclidianF is a base functor of Euclidian
 euclidian :: (a -> c) -> (b -> c) -> EuclidianF a b -> c
 euclidian f g = u
   where u (TrivF n) = f n
         u (SameF n) = g n
 
-sort :: (Show a, Ord a) => [a] -> [a]
-sort xs = hylo phi (psi $?) ([], xs)
-
 phi :: EuclidianF a a -> a
 phi = euclidian id id
 
-psi :: Ord a => ([a], [a]) -> EuclidianF [a] ([a], [a])
+psi :: Ord a => ([a], [a]) -> EuclidianF ([a], [a]) ([a], [a])
 psi (xs, ys) = case ys of
-  []     -> TrivF xs
+  []     -> TrivF (xs, [])
   (y:ys) -> SameF (zs++[w], ws)
     where (z, zs) = swap (y, xs)
           (w, ws) = swap (z, ys)
@@ -45,3 +39,11 @@ swap (x, xs) = case break (x<) xs of
     where (x, xs) <+> (y, ys) = (y, xs++[x]++ys)
 
 sample = [1,3,2,5,4,7,6,0]
+{--
+sort :: (Show a, Ord a) => [a] -> [a]
+sort xs = hylo phi (psi $?) ([], xs)
+
+
+
+
+--}
