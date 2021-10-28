7862c666f3fdffa5fee1c115bc839725fc5400ab
cutsea110 <cutsea110@gmail.com>
2021-10-28 20:31:24 +0900

    use recursion-schemes

--- src/Treemap.hs
+++ src/Treemap.hs
@@ -1,7 +1,11 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TemplateHaskell, TypeFamilies, KindSignatures #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# LANGUAGE LambdaCase, BangPatterns #-}
 module Treemap where
 
 import Data.Monoid
+import Data.Functor.Foldable
+import Data.Functor.Foldable.TH
 
 type Label = String
 
@@ -17,45 +21,12 @@ type Label = String
 -}
 data Treemap a = Leaf a
                | Node Label (Forest a)
-               deriving (Show, Functor)
+               deriving (Show, Functor, Foldable)
 
-newtype Forest a = Grows [Treemap a] deriving (Show, Functor)
+newtype Forest a = Grows [Treemap a] deriving (Show, Functor, Foldable)
 
-outt :: Treemap a -> Either a (Label, Forest a)
-outt (Leaf x) = Left x
-outt (Node l fs) = Right (l, fs)
-
-outf :: Forest a -> Maybe (Treemap a, Forest a)
-outf (Grows [])     = Nothing
-outf (Grows (t:ts)) = Just (t, Grows ts)
-
-foldt :: (a -> c) -> (Label -> b -> c) -> b -> (c -> b -> b) -> Treemap a -> c
-foldt f g c h = u
-  where u (Leaf x)    = f x
-        u (Node l ts) = g l (v ts)
-        v = foldf f g c h
-
-unfoldt :: (b -> Either a (Label, c)) -> (c -> Maybe (b, c)) -> b -> Treemap a
-unfoldt psit psif t = case psit t of
-  Left x        -> Leaf x
-  Right (l, fs) -> Node l (unfoldf psit psif fs)
-
-foldf :: (a -> c) -> (Label -> b -> c) -> b -> (c -> b -> b) -> Forest a -> b
-foldf f g c h = v
-  where u = foldt f g c h
-        v (Grows [])     = c
-        v (Grows (t:ts)) = h (u t) (v (Grows ts))
-
-unfoldf :: (b -> Either a (Label, c)) -> (c -> Maybe (b, c)) -> c -> Forest a
-unfoldf psit psif f = case psif f of
-  Nothing -> Grows []
-  Just (t, ts) -> let Grows fs = unfoldf psit psif ts
-                  in Grows (unfoldt psit psif t : fs)
-
-sumt :: Treemap (Sum Integer) -> Sum Integer
-sumt = foldt id (const id) 0 (+)
-sumf :: Forest (Sum Integer) -> Sum Integer
-sumf = foldf id (const id) 0 (+)
+makeBaseFunctor ''Treemap
+makeBaseFunctor ''Forest
 
 sample :: Treemap Integer
 sample = Node "cluster1"
