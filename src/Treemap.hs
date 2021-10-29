afb2454bb85c3b545979dc3680929094f506c5ac
cutsea110 <cutsea110@gmail.com>
2021-10-29 11:39:14 +0900

    more simple

--- src/Treemap.hs
+++ src/Treemap.hs
@@ -3,46 +3,40 @@
 {-# LANGUAGE LambdaCase, BangPatterns #-}
 module Treemap where
 
+import Control.Comonad.Cofree
 import Data.Monoid
 import Data.Functor.Foldable
 import Data.Functor.Foldable.TH
 
 type Label = String
 
-{- |
-           [Leaf,Node] x [Nil,Cons]
-   Ta x Fa <------------------ (A + Fa) x (1 + Ta x Fa)
-    |   |                         |          |
-  u |   | v                       | 1 + v    | 1 + u x v
-    |   |                         |          |
-    v   v                         v          v
-    X x Y  <------------------ (A + Y ) x (1 +  X x Y )
-            [f, g] x [c, h]
--}
 data Treemap a = Leaf a
-               | Node Label (Forest a)
+               | Node Label [Treemap a]
                deriving (Show, Functor, Foldable)
 
-newtype Forest a = Grows [Treemap a] deriving (Show, Functor, Foldable)
-
 makeBaseFunctor ''Treemap
-makeBaseFunctor ''Forest
+
+-- phi :: TreemapF a (Cofree (TreemapF a) b) -> b
+phi (LeafF x) = x
+phi (NodeF l ts) = sum (fmap extract ts)
+
+extract :: Cofree f a -> a
+extract (x :< _) = x
+
+sub :: Cofree f a -> f (Cofree f a)
+sub (_ :< xs) = xs
 
 sample :: Treemap Integer
 sample = Node "cluster1"
-         (Grows
-           [ Node "cluster2"
-             (Grows
-               [ Node "cluster4"
-                 (Grows
-                   [ Leaf 2
-                   , Leaf 3
-                   ])
-               , Leaf 5
-               ])
-           , Node "cluster3"
-             (Grows
-               [ Leaf 10
-               , Leaf 5
-               ])
-           ])
+         [ Node "cluster2"
+           [ Node "cluster4"
+             [ Leaf 2
+             , Leaf 3
+             ]
+           , Leaf 5
+           ]
+         , Node "cluster3"
+           [ Leaf 10
+           , Leaf 5
+           ]
+         ]
