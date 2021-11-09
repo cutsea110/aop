9b28d8e29c650bd6350caf132c1ea09e60738577
cutsea110 <cutsea110@gmail.com>
2021-11-10 07:43:21 +0900

    introduction: insertSort

--- src/DualityOfSorts.hs
+++ src/DualityOfSorts.hs
@@ -1,3 +1,9 @@
 module DualityOfSorts where
 -- ref.) Duality of Sorts by Rafl Hinze
 
+insertSort :: [Integer] -> [Integer]
+insertSort = foldr insert []
+
+insert :: Integer -> [Integer] -> [Integer]
+insert y ys = xs ++ [y] ++ zs
+  where (xs, zs) = span (<=y) ys
