bebb4b425149ba75f8f3cef7bc9ffecc10a5cb83
cutsea110 <cutsea110@gmail.com>
2021-11-10 07:45:55 +0900

    introduction: selectSort

--- src/DualityOfSorts.hs
+++ src/DualityOfSorts.hs
@@ -1,9 +1,20 @@
 module DualityOfSorts where
 -- ref.) Duality of Sorts by Rafl Hinze
 
+import Data.List (delete, unfoldr)
+
 insertSort :: [Integer] -> [Integer]
 insertSort = foldr insert []
 
 insert :: Integer -> [Integer] -> [Integer]
 insert y ys = xs ++ [y] ++ zs
   where (xs, zs) = span (<=y) ys
+
+selectSort :: [Integer] -> [Integer]
+selectSort = unfoldr select
+
+select :: [Integer] -> Maybe (Integer, [Integer])
+select [] = Nothing
+select xs = Just (x, xs')
+  where x   = minimum xs
+        xs' = delete x xs
