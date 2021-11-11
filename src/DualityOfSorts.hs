70c02c66c05f9a300cc9baca259fb3fb41fd8cb3
cutsea110 <cutsea110@gmail.com>
2021-11-11 11:15:43 +0900

    refactor: pretty debug trace

--- src/DualityOfSorts.hs
+++ src/DualityOfSorts.hs
@@ -7,25 +7,21 @@ import Data.List (delete, unfoldr)
 --------------------------------------------------------------------------------------
 debug = True
 
-($?) :: Show a => (a -> b) -> a -> b
-f $? x = if debug then trace (show x) (f x) else f x
+($?) :: (Show a, Show b) => (a -> b) -> a -> b
+f $? x = let v = f x
+             msg = " {- " ++ show x ++ " => " ++ show v ++ " -}"
+         in if debug then trace msg v else v
 --------------------------------------------------------------------------------------
 
 insertSort :: [Integer] -> [Integer]
-insertSort = foldr phi []
-  where phi x xs = let v = insert (x, xs)
-                       msg = " {- " ++ show (x, xs) ++ " => " ++ show v ++ " -}"
-                   in trace msg v
+insertSort = foldr (curry (insert $?)) []
 
 insert :: (Integer, [Integer]) -> [Integer]
 insert (y, ys) = xs ++ [y] ++ zs
   where (xs, zs) = span (<=y) ys
 
 selectSort :: [Integer] -> [Integer]
-selectSort = unfoldr psi
-  where psi x = let v = select x
-                    msg = " {- " ++ show x ++ " => " ++ show v ++ " -}"
-                in trace msg v
+selectSort = unfoldr (select $?)
 
 select :: [Integer] -> Maybe (Integer, [Integer])
 select [] = Nothing
