a0b2eaaa3e3fc5e862687a9cc7d2501ef0533ae3
cutsea110 <cutsea110@gmail.com>
2021-11-17 18:47:25 +0900

    mergeSort

--- src/DualityOfSorts.hs
+++ src/DualityOfSorts.hs
@@ -187,3 +187,76 @@ merge []     bs = bs
 merge (a:as) (b:bs)
   | a <= b    = a:merge as (b:bs)
   | otherwise = b:merge (a:as) bs
+
+data Tree tree = Tip | Leaf Integer | Fork tree tree deriving Show
+instance Functor Tree where
+  fmap f Tip = Tip
+  fmap f (Leaf a) = Leaf a
+  fmap f (Fork l r) = Fork (f l) (f r)
+
+--------------------------------------------------------------------------------------
+-- Section 6.1
+
+data List' list = Nil' | Single' Integer | Cons' Integer list deriving Show
+instance Functor List' where
+  fmap f Nil' = Nil'
+  fmap f (Single' a) = Single' a
+  fmap f (Cons' a as) = Cons' a (f as)
+
+nil' :: Fix List'
+nil' = In Nil'
+single' :: Integer -> Fix List'
+single' a = In (Single' a)
+cons' :: Integer -> Fix List' -> Fix List'
+cons' a as = In (Cons' a as)
+
+fromList' :: Fix List' -> [Integer]
+fromList' (In Nil') = []
+fromList' (In (Single' a)) = [a]
+fromList' (In (Cons' a as)) = a:fromList' as
+
+sample' :: Fix List'
+sample' = cons' 3 (cons' 1 (cons' 4 (single' 2)))
+
+grow :: List' (Tree $*$ x) -> Tree (List' $+$ x)
+grow Nil' = Tip
+grow (Single' a) = Leaf a
+grow (Cons' a (t, Tip)) = Leaf a
+grow (Cons' a (t, Leaf b)) = Fork (Go (Single' a)) (Stop t)
+-- grow (Cons' a (t, Fork l r)) = Fork (Go (Cons' a l)) (Stop r)
+grow (Cons' a (t, Fork l r)) = Fork (Go (Cons' a r)) (Stop l)
+
+makeTree :: Fix List' -> Fix Tree
+makeTree = fold (apo (grow . fmap (pair id out)))
+makeTree' :: Fix List' -> Fix Tree
+makeTree' = unfold (para (fmap (join id In) . grow))
+
+--------------------------------------------------------------------------------------
+-- Section 6.2
+
+merge' :: Tree (SList $*$ x) -> SList (Tree $+$ x)
+merge' Tip = SNil
+merge' (Leaf a) = SCons a (Go Tip)
+merge' (Fork (l, SNil)       (r, SNil))       = SNil
+merge' (Fork (l, SNil)       (r, SCons b r')) = SCons b (Stop r')
+merge' (Fork (l, SCons a l') (r, SNil))       = SCons a (Stop l')
+merge' (Fork (l, SCons a l') (r, SCons b r'))
+  | a <= b    = SCons a (Go (Fork l' r))
+  | otherwise = SCons b (Go (Fork l r'))
+
+mergeTree :: Fix Tree -> Fix SList
+mergeTree = fold (apo (merge' . fmap (pair id out)))
+mergeTree' :: Fix Tree -> Fix SList
+mergeTree' = unfold (para (fmap (join id In) . merge'))
+
+--------------------------------------------------------------------------------------
+-- Section 6.3
+
+mergeSort1 :: Fix List' -> Fix SList
+mergeSort1 = mergeTree . makeTree
+mergeSort2 :: Fix List' -> Fix SList
+mergeSort2 = mergeTree . makeTree'
+mergeSort3 :: Fix List' -> Fix SList
+mergeSort3 = mergeTree' . makeTree
+mergeSort4 :: Fix List' -> Fix SList
+mergeSort4 = mergeTree' . makeTree'
