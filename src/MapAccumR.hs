9895fc82ad3c847670012cd1c7f3ffa3ad49c3ff
cutsea110 <cutsea110@gmail.com>
2022-09-29 09:56:14 +0900

    select

--- src/MapAccumR.hs
+++ src/MapAccumR.hs
@@ -2,8 +2,9 @@
 module MapAccumR where
 
 import Control.Arrow ((***), (&&&), first, second)
+import Curry (phi)
 
--- |
+-- | cata for list
 --                   [nil, cons]
 --           Ta <----------------- 1 + A x Ta
 --           |                       |
@@ -16,6 +17,30 @@ cata :: (b, (a, b) -> b) -> [a] -> b
 cata (c, f) = u
   where u [] = c
         u (x:xs) = f (x, u xs)
+
+-- | ana for snoc list
+--                   alpha
+--           Tb <----------------- 1 + Tb x B
+--           ^                       ^
+--   [(psi)] |                       | id + [(psi)] x ida
+--           |                       |
+--           X  -----------------> 1 + X x B
+--                    psi
+--
+ana :: (a -> Maybe (a, b)) -> a -> [b]
+ana psi = v
+  where v x = case psi x of
+          Nothing     -> []
+          Just (xs, x) -> snoc (v xs) x
+
+snoc :: [a] -> a -> [a]
+snoc xs x = xs ++ [x]
+
+-- | reverse on snoc list
+rev :: [a] -> [a]
+rev = ana psi
+  where psi []     = Nothing
+        psi (x:xs) = Just (xs, x)
 
 -- |
 --                            [nil, cons]                                    *   x   xs
@@ -31,11 +56,7 @@ cata (c, f) = u
 --
 mapAccumR :: (x, (x, a) -> (x, b)) -> [a] -> (x, [b])
 mapAccumR (s, f) = cata ((s,[]), second cons . assocr . first (f . swap) . assocl)
-{--
-mapAccumR f s = u
-  where u [] = (s, [])
-        u (x:xs) = (second (uncurry (:)) . assocr . first (f . swap) . assocl) (x, u xs)
---}
+
 cons :: (a, [a]) -> [a]
 cons (x, xs) = x:xs
 assocl ::  (a, (b, c)) -> ((a, b), c)
@@ -44,3 +65,12 @@ assocr :: ((a, b), c) -> (a, (b, c))
 assocr ((x, b), y) = (x, (b, y))
 swap :: (a, b) -> (b, a)
 swap (x, y) = (y, x)
+
+
+-- | select?
+-- TODO: we can rewrite by using hylo because mapAccumR is cata and rev is ana
+select :: Ord a => (a, [a]) -> (a, [a])
+select (x, xs) = mapAccumR (x, f) (rev xs)
+  where
+    f (a, b) | a > b     = (b, a)
+             | otherwise = (a, b)
