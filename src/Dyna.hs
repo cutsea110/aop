20fb27a5212704fe18b701877c3b6f17c649793f
cutsea110 <cutsea110@gmail.com>
2023-03-30 09:29:38 +0900

    imple

--- src/Dyna.hs
+++ src/Dyna.hs
@@ -45,8 +45,81 @@ module Dyna where
 --
 -- F*(X) = A * F(X)
 -- vF* = A * F(vF*)
+--
 -- ex.)
 --   F  == Maybe ==> F(X) = 1 + X
 --  uF  == Nat  Z, S Z, S (S Z) ...
 --  F*  == A * Maybe ==> F*(X) = A * F(X) = A * (1 + X) == A + A * X
 --  vF* == NonEmptyList A
+data Nat = Z | S Nat deriving Show
+
+--             [z, s]
+--    Nat <-------------- 1 + Nat
+--      |                  |
+--   u  |                  | id + u
+--      v                  v
+--      X <-------------- 1 + X
+--             [c, f]
+foldn :: (a, a -> a) -> Nat -> a
+foldn (c, f) = u
+  where u Z = c
+        u (S n) = f (u n)
+
+--              out
+--    Nat --------------> 1 + Nat
+--      ^                  ^
+--   v  |                  | id + v
+--      |                  |
+--      X --------------> 1 + X
+--               psi
+unfoldn :: (a -> Maybe a) -> a -> Nat
+unfoldn psi = v
+  where v x = case psi x of
+          Nothing -> Z
+          Just x' -> S (v x')
+
+out :: Nat -> Maybe Nat
+out Z     = Nothing
+out (S n) = Just n
+
+pair :: (a -> b) -> (a -> c) -> a -> (b, c)
+pair f g x = (f x, g x)
+
+hd :: NEList a -> a
+hd (Unit x) = x
+hd (Cons x _) = x
+
+tl :: NEList a -> Maybe (NEList a)
+tl (Unit _)   = Nothing
+tl (Cons _ xs) = Just xs
+
+data NEList a = Unit a | Cons a (NEList a) deriving Show
+
+--                    [z, s]
+--          Nat <---------------------- 1 + Nat
+--          / |            A  <-----+    |
+-- hist phi/  | u          ^   phi   \   | id + u == fmap u
+--        /   v            |          \  v
+--       A <- [A]+ <- A * (1 + [A]+) <- 1 + [A]+
+--          e     In*      | <phi,id> /
+--                         v         /
+--                    1 + [A]+ <----+
+--                               id
+--
+hist :: (Maybe (NEList a) -> a) -> Nat -> a
+hist phi = hd . u
+  where u n = maybe (Unit v) (Cons v) m
+          where
+            m = fmap u (out n) -- fmap is Maybe's
+            v = phi m
+
+dyna :: (Maybe (NEList c) -> c) -> (a -> Maybe a) -> a -> c
+dyna f g = hist f . unfoldn g
+
+
+fib = dyna phi psi
+  where
+    phi = maybe 0 f
+      where f x = maybe 1 (\xs -> hd x + hd xs) (tl x)
+
+    psi n = if n == 0 then Nothing else Just (n-1)
