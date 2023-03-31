bdf5129c4d883573b48c528e207408abf1b619ce
cutsea110 <cutsea110@gmail.com>
2023-04-01 08:54:18 +0900

    restart story

--- src/Dyna.hs
+++ src/Dyna.hs
@@ -1,1 +1,48 @@
 module Dyna where
+
+
+data Nat = Z | S Nat deriving Show
+
+--
+-- Cata
+--
+--
+--         [z, s]
+-- Nat <----------- 1 + Nat
+--   |               |
+-- u |               |  id + u
+--   v               v
+--   X <----------- 1 + X
+--         [c, f]
+--
+foldn (c, f) = u
+  where u Z = c
+        u (S n) = f (u n)
+
+plus x = foldn (x, S)
+times x = foldn (Z, plus x)
+exp x = foldn (S Z, times x)
+toInt = foldn (0, (1+))
+
+--
+-- Ana
+--
+--
+--         out
+-- Nat -----------> 1 + Nat
+--   ^               ^
+-- v |               |  id + v
+--   |               |
+--   X <----------- 1 + X
+--          psi
+--
+unfoldn psi = v
+  where v x = case psi x of
+          Nothing -> Z
+          Just x' -> S (v x')
+
+fromInt = unfoldn (\x -> if x <= 0 then Nothing else Just (x-1))
+
+--
+-- Hist
+--
