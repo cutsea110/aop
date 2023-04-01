6f696e378f9e1a6b8acb8102230e284c97fd6f4f
cutsea110 <cutsea110@gmail.com>
2023-04-01 11:05:21 +0900

    impl hist

--- src/Dyna.hs
+++ src/Dyna.hs
@@ -1,7 +1,10 @@
 module Dyna where
 
-
 data Nat = Z | S Nat deriving Show
+
+out :: Nat -> Maybe Nat
+out Z     = Nothing
+out (S n) = Just n
 
 --
 -- Cata
@@ -46,3 +49,40 @@ fromInt = unfoldn (\x -> if x <= 0 then Nothing else Just (x-1))
 --
 -- Hist
 --
+-- F(X) = 1 + X
+-- uF == F(uF) = 1 + uF
+-- F*(X) = A * F(X) = A * 1 + X = A + A * X
+-- vF* == A + A * vF*
+data NEList a = Unit a | Cons a (NEList a) deriving Show
+
+in' :: (a, Maybe (NEList a)) -> NEList a
+in' (x, Nothing) = Unit x
+in' (x, Just xs) = Cons x xs
+
+hd :: NEList a -> a
+hd (Unit x)   = x
+hd (Cons x _) = x
+
+tl :: NEList a -> Maybe (NEList a)
+tl (Unit _)    = Nothing
+tl (Cons _ xs) = Just xs
+
+--
+--                          [z, s]
+--          N  <----------------------------- 1 + N
+--         /|                                   |
+--        / |                                   |
+--  hist /  | u              A <----+           | id + u
+--      /   |                |       \  phi     |
+--     /    v                v        +------ , v
+--    A <-- [A]+ <---- A * (1 + [A]+) <------ 1 + [A]+
+--       e         In*       ^        +-------'
+--                           |       /
+--                       1 + [A]+ <-+
+--
+hist phi = hd . u
+  where u n = in' (v, m)
+          where m = fmap u (out n)
+                v = phi m
+
+------
