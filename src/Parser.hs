e633bceb674bf3396306704c4254bc6caaa3bbc6
cutsea110 <cutsea110@gmail.com>
2023-07-01 15:37:59 +0900

    modify parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,10 +1,10 @@
 module Parser where
 
-import Data.Char (isDigit, isAlpha, isAlphaNum)
+import Data.Char (isDigit, isSpace)
 
 type Location = Int
 type Token = (Location, String)
-newtype Parser a = Parser { runParser :: [Token] -> [(a, [Token])] }
+newtype Parser a = Parser { runParser :: [Token] -> [(a, [Token])]}
 
 pSat :: (String -> Bool) -> Parser String
 pSat p = Parser f
@@ -15,11 +15,21 @@ pSat p = Parser f
 pLit :: String -> Parser String
 pLit s = pSat (== s)
 
-pApply :: Parser a -> (a -> b) -> Parser b
-p `pApply` f = Parser (\toks -> [(f v, toks') | (v, toks') <- runParser p toks])
+pEmpty :: a -> Parser a
+pEmpty x = Parser (\toks -> [(x, toks)])
 
+pBind :: Parser a -> (a -> Parser b) -> Parser b
+px `pBind` f = Parser (\toks -> [ (v, toks'')
+                                | (x, toks') <- runParser px toks
+                                , (v, toks'') <- runParser (f x) toks'
+                                ])
+
+pApply :: (a -> b) -> Parser a -> Parser b
+f `pApply` p = Parser (\toks -> [ (f x, toks')
+                                | (x, toks') <- runParser p toks
+                                ])
 pNum :: Parser Int
-pNum = pSat (all isDigit) `pApply` read
+pNum = read `pApply` pSat (all isDigit)
 
 pAlt :: Parser a -> Parser a -> Parser a
 p `pAlt` q = Parser (\toks -> runParser p toks ++ runParser q toks)
@@ -28,59 +38,64 @@ pAltL :: Parser a -> Parser a -> Parser a
 p `pAltL` q = Parser (\toks -> runParser p toks <+ runParser q toks)
   where
     [] <+ ys = ys
-    xs <+ _ = xs
+    xs <+ _  = xs
 
-pThen :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
-pThen op p q = Parser f
-  where
-    f toks = [ (x `op` y, toks'')
-             | (x, toks') <- runParser p toks
-             , (y, toks'') <- runParser q toks'
-             ]
 
-pVar :: Parser String
-pVar = pSat p
-  where
-    p (c:cs) = isAlpha c && all isAlphaNum cs
-    p [] = error "Empty string"
+pAp :: Parser (a -> b) -> Parser a -> Parser b
+pf `pAp` px = Parser (\toks -> [ (f v, toks'')
+                               | (f, toks') <- runParser pf toks
+                               , (v, toks'') <- runParser px toks'
+                               ])
 
-pEmpty :: a -> Parser a
-pEmpty x = Parser (\toks -> [(x, toks)])
+pApply2 :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+pApply2 f p q = Parser (\toks -> [ (f x y, toks'')
+                                 | (x, toks') <- runParser p toks
+                                 , (y, toks'') <- runParser q toks'
+                                 ])
 
 pOneOrMore :: Parser a -> Parser [a]
-pOneOrMore p = pThen (:) p (pZeroOrMore p)
+pOneOrMore p = pApply2 (:) p (pZeroOrMore p)
 
 pZeroOrMore :: Parser a -> Parser [a]
 pZeroOrMore p = pOneOrMore p `pAlt` pEmpty []
 
-pOneOrMoreWithSep :: Parser sep -> Parser a -> Parser [a]
-pOneOrMoreWithSep sep p = pThen (:) p (pZeroOrMore (pThen (flip const) sep p))
-
 pMunch1 :: Parser a -> Parser [a]
-pMunch1 p = pThen (:) p (pMunch p)
+pMunch1 p = pApply2 (:) p (pMunch p)
 
 pMunch :: Parser a -> Parser [a]
 pMunch p = pMunch1 p `pAltL` pEmpty []
 
-pAp :: Parser (a -> b) -> Parser a -> Parser b
-pAp pf px = Parser (\toks -> [ (f x, toks'')
-                             | (f, toks') <- runParser pf toks
-                             , (x, toks'') <- runParser px toks'
-                             ])
+pOneOrMoreWithSep :: Parser sep -> Parser a -> Parser [a]
+pOneOrMoreWithSep sep p = pApply2 (:) p (pZeroOrMore (sep *> p))
 
-pBind :: Parser a -> (a -> Parser b) -> Parser b
-px `pBind` f = Parser (\toks -> [ (y, toks'')
-                                | (x, toks') <- runParser px toks
-                                , (y, toks'') <- runParser (f x) toks'
-                                ])
+pMunch1WithSep :: Parser sep -> Parser a -> Parser [a]
+pMunch1WithSep sep p = pApply2 (:) p (pMunch (sep *> p))
 
 instance Functor Parser where
-  fmap = flip pApply
+  fmap = pApply
 
 instance Applicative Parser where
   pure = pEmpty
   (<*>) = pAp
 
 instance Monad Parser where
-  (>>=) = pBind
   return = pure
+  (>>=) = pBind
+
+-------
+
+fileName :: Parser String
+fileName = concat <$> pMunch1 (pSat (/=":"))
+mimeType :: Parser String
+mimeType = concat <$> pMunch1 (pSat (/="\n"))
+sep :: Parser ()
+sep = () <$ (pLit ":" *> pMunch (pSat (all isSpace)))
+line :: Parser (String, String)
+line = (,) <$> fileName <* sep <*> mimeType
+content = pMunch1WithSep (pLit "\n") line <* pMunch (pSat (all isSpace))
+
+main :: IO [(String, String)]
+main = do
+  inp <- readFile "mime.txt"
+  let toks = concatMap (\c -> [(1, [c])]) inp
+  return . fst . head . runParser content $ toks
