8267cb96494b510e968a5d7d3c1a5b1af4e1b1f8
cutsea110 <cutsea110@gmail.com>
2023-07-17 09:59:06 +0900

    support some p-digit number

--- src/Decimal.hs
+++ src/Decimal.hs
@@ -1,13 +1,22 @@
 module Decimal where
 
-{--
-val' b m
-  | m < b     = [m]
-  | otherwise = val' b (m `div` b) ++ [m `mod` b]
---}
+val :: Int -> [Int] -> Int
+val b = foldl phi 0
+  where
+    phi n d = n * b + d
 
+val' :: Int -> Int -> [Int]
 val' b m = f (m, [])
   where
     f (m, xs)
       | m < b     = m:xs
       | otherwise = f (m `div` b, m `mod` b:xs)
+
+bin  = val  2
+bin' = val' 2
+oct  = val  8
+oct' = val' 8
+dec  = val  10
+dec' = val' 10
+hex  = val  16
+hex' = val' 16
