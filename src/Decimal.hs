0cf625fd11bbeb4c2cc8d8cdd1e418bdbf507146
cutsea110 <cutsea110@gmail.com>
2023-07-17 10:13:13 +0900

    refactor

--- src/Decimal.hs
+++ src/Decimal.hs
@@ -1,5 +1,7 @@
 module Decimal where
 
+import Data.Char (chr, ord)
+
 val :: Int -> [Int] -> Int
 val b = foldl phi 0
   where
@@ -12,11 +14,19 @@ val' b m = f (m, [])
       | m < b     = m:xs
       | otherwise = f (m `div` b, m `mod` b:xs)
 
-bin  = val  2
+bin, oct, dec, hex :: [Int] -> Int
+bin = val 2
+oct = val 8
+dec = val 10
+hex = val 16
+
+bin', oct', dec', hex' :: Int -> [Int]
 bin' = val' 2
-oct  = val  8
 oct' = val' 8
-dec  = val  10
 dec' = val' 10
-hex  = val  16
 hex' = val' 16
+
+toChar :: Int -> Char
+toChar n
+  | 0 <= n && n <= 9 = chr (ord '0' + n)
+  | 10 <= n && n <= 15 = chr (ord 'a' + n - 10)
