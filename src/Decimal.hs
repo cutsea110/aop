b9144051395be9ef30720ab6e6522f6dac022f4a
cutsea110 <cutsea110@gmail.com>
2023-07-18 14:42:20 +0900

    high speed exp

--- src/Decimal.hs
+++ src/Decimal.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE NPlusKPatterns #-}
 module Decimal where
 
-import Prelude hiding (exp)
+import Prelude hiding (exp, pred)
 import Data.Char (chr, ord)
 import ExtEuclid (psi)
 
@@ -31,11 +31,11 @@ hex' = val' 16
 
 toChar :: Int -> Char
 toChar n
-  | 0 <= n && n <= 9 = chr (ord '0' + n)
+  | 0  <= n && n <= 9  = chr (ord '0' + n)
   | 10 <= n && n <= 15 = chr (ord 'a' + n - 10)
 
 
-data Nat = Z | S Nat deriving (Show, Eq)
+data Nat = Z | S Nat deriving (Show, Eq, Ord)
 foldn c f = u
   where u Z = c
         u (S n) = f (u n)
@@ -53,6 +53,18 @@ fromInt = unfoldn psi
   where psi 0     = Nothing
         psi (n+1) = Just n
 
+pred :: Nat -> Nat
+pred Z     = Z
+pred (S n) = n
+
+sub :: Nat -> Nat -> Nat
+sub a = foldn a pred
+divide :: Nat -> Nat -> Nat
+a `divide` b = f Z a b
+  where f d a b
+          | a < b     = d
+          | otherwise = f (S d) (a `sub` b) b
+
 add a b = foldn b S a
 mult a = foldn Z (add a)
 exp a = foldn (S Z) (mult a)
@@ -67,3 +79,21 @@ data Bin = Nil | Snoc Bin Bit deriving (Eq, Show)
 foldbin c f = u
   where u Nil = c
         u (Snoc b x) = f (u b) x
+
+convert :: Bin -> Nat
+convert = foldbin zero shift
+  where zero = Z
+        shift n d = (S (S Z) `mult` n) `add` case d of
+          O -> Z
+          I -> S Z
+
+convert' :: Nat -> Bin
+convert' = undefined
+
+-- | high speed exponentiation
+exp' :: Nat -> Nat -> Nat
+exp' base = foldbin one (op base) . convert'
+  where one = S Z
+        op a n d
+          | d == O    = n `mult` n
+          | otherwise = a `mult` (n `mult` n)
