3c75e92dbfff5719758abe4d991a56d10b8bbf32
cutsea110 <cutsea110@gmail.com>
2023-09-01 22:20:21 +0900

    refactor

--- src/Transaction.hs
+++ src/Transaction.hs
@@ -8,68 +8,52 @@ import Control.Monad.Trans.Class (MonadTrans(..))
 import Control.Monad.IO.Class (MonadIO(..))
 
 newtype TransactionT ctx m a = TxT { runT :: ctx -> m (a, ctx) }
+
 instance MonadTrans (TransactionT ctx) where
-  lift m = TxT (\ctx -> do
-                   x <- m
-                   return (x, ctx))
+  lift m = TxT $ \ctx ->
+    do
+      x <- m
+      return (x, ctx)
+
 instance Monad m => Monad (TransactionT cxt m) where
-  tx >>= f = TxT (\ctx -> do
-                      (x, ctx') <- runT tx ctx
-                      runT (f x) ctx')
+  tx >>= f = TxT $ \ctx ->
+    do
+      (x, ctx') <- runT tx ctx
+      runT (f x) ctx'
 instance Monad m => Functor (TransactionT ctx m) where
-  fmap f tx = TxT (\ctx -> do
-                      (x, ctx') <- runT tx ctx
-                      return (f x, ctx'))
+  fmap f tx = TxT $ \ctx ->
+    do
+      (x, ctx') <- runT tx ctx
+      return (f x, ctx')
 
 instance Monad m => Applicative (TransactionT ctx m) where
   pure x = TxT (\ctx -> return (x, ctx))
-  f <*> tx = TxT (\ctx -> do
-                       (f', ctx') <- runT f ctx
-                       (x, ctx'') <- runT tx ctx'
-                       return (f' x, ctx''))
+  f <*> tx = TxT $ \ctx ->
+    do
+      (f', ctx') <- runT f ctx
+      (x, ctx'') <- runT tx ctx'
+      return (f' x, ctx'')
 
 instance MonadIO m => MonadIO (TransactionT ctx m) where
   liftIO = lift . liftIO
 
 instance MonadFail m => MonadFail (TransactionT ctx m) where
-  fail msg = TxT (\ctx -> fail $ "TransactionT: failed\n" ++ msg)
+  fail msg = TxT $ \ctx ->
+    fail $ "TransactionT: failed\n" ++ msg
 
 class Monad m => MonadTransaction ctx m | m -> ctx where
-  begin :: ctx -> m a
-  commit :: m a -> m ()
-  rollback :: m a -> m ()
+  txEmpty :: a -> m a
+  txEmpty = return
+  txBind :: m a -> (a -> m b) -> m b
+  txBind = (>>=)
+  txApply :: (a -> b) -> m a -> m b
+  txApply = (<$>)
+  txApply2 :: (a -> b -> c) -> m a -> m b -> m c
+  txApply2 f tx1 tx2 = do
+    x <- tx1
+    y <- tx2
+    return $ f x y
+  txAp :: m (a -> b) -> m a -> m b
+  txAp = (<*>)
 
-type Transaction ctx a = TransactionT ctx IO a
-
-{-
-newtype Transaction ctx e a = Tx { run :: ctx -> Either e (a, ctx) }
-
--- return
-txEmpty :: a -> Transaction ctx e a
-txEmpty x = Tx (\ctx -> Right (x, ctx))
-
--- (=<<)
-andThen :: Transaction ctx e a -> (a -> Transaction ctx e b) -> Transaction ctx e b
-tx `andThen` f = Tx g
-  where g ctx = run <$> f . fst <*> snd =<< run tx ctx
-
-txApply :: (a -> b) -> Transaction ctx e a -> Transaction ctx e b
-f `txApply` tx = Tx g
-  where g ctx = first f <$> run tx ctx
-
-txApply2 :: (a -> b -> c) -> Transaction ctx e a -> Transaction ctx e b -> Transaction ctx e c
-txApply2 f tx1 tx2 = Tx g
-  where g ctx = do
-          (x, ctx')  <- run tx1 ctx
-          (y, ctx'') <- run tx2 ctx'
-          return (f x y, ctx'')
-
-txAp :: Transaction ctx e (a -> b) -> Transaction ctx e a -> Transaction ctx e b
-tf `txAp` tx = txApply2 ($) tf tx
-
-txAlt :: Transaction ctx e a -> Transaction ctx e a -> Transaction ctx e a
-tx1 `txAlt` tx2 = Tx g
-  where g ctx = case run tx1 ctx of
-          Left _ -> run tx2 ctx
-          r      -> r
--}
+instance Monad m => MonadTransaction ctx (TransactionT ctx m)
