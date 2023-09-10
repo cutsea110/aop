04fdac94a6646d1c627467fe4f398fe3833aeb1b
cutsea110 <cutsea110@gmail.com>
2023-09-10 10:12:30 +0900

    refactor

--- src/Dijkstra.hs
+++ src/Dijkstra.hs
@@ -50,7 +50,7 @@ data Graph = Empty
 -}
 
 -- | Graph
-data Graph = Empty | Context :&: Graph deriving (Show, Eq)
+data Graph = Empty | Context :&: Graph deriving Show
 type Context = ([Vertex], Vertex, [Vertex])
 type Vertex = String
 infixr 5 :&:
@@ -107,20 +107,6 @@ apm v g = sub g ([],[])
         sub ((p1,v1,s1) :&: g1) (p,s)
           | v == v1   = sub g1 (p1++p, s1++s)
           | otherwise = let ((p2,_,s2), g2) = sub g1 (p,s)
-                            (p3,s2') = if v `elem` p1
-                                       then (delete v p1, nub $ v1:s2)
-                                       else (p1,s2)
-                            (s3,p2') = if v `elem` s1
-                                       then (delete v s1, nub $ v1:p2)
-                                       else (s1,p2)
-                        in ((p2',v,s2'), (p3,v1,s3) :&: g2)
-
-apm' :: Vertex -> Graph -> (Context, Graph)
-apm' v g = sub g ([],[])
-  where sub Empty (p,s) = ((p,v,s), Empty)
-        sub ((p1,v1,s1) :&: g1) (p,s)
-          | v == v1   = sub g1 (p1++p, s1++s)
-          | otherwise = let ((p2,_,s2), g2) = sub g1 (p,s)
                             (p3,s2') = delCons (v, v1) (p1, s2)
                             (s3,p2') = delCons (v, v1) (s1, p2)
                         in ((p2',v,s2'), (p3,v1,s3) :&: g2)
@@ -133,11 +119,6 @@ del v (x:xs)
 
 delCons :: Eq a => (a, b) -> ([a],[b]) -> ([a],[b])
 delCons (x, y) (xs, ys) = maybe (xs, ys) (\xs' -> (xs', y:ys)) $ del x xs
-
--- | for apm refactor
-test28 = all (\v -> apm v g28 == apm' v g28) ["a","b","c","d","e"]
-test32 = all (\v -> apm v g32 == apm' v g32) ["1","2","3","4","5","6","7"]
-
 
 -- | Graph examples
 g28 = (["d"],"a",["b"]) :&: ([],"b",["c","d"]) :&: ([],"c",[]) :&: ([],"d",[]) :&: Empty
