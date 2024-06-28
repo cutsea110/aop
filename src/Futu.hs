80d3ff9b08fc0d93ec74f203486d7e27b7a3e9be
cutsea110 <cutsea110@gmail.com>
2024-06-28 12:19:03 +0900

    trace

--- src/Futu.hs
+++ src/Futu.hs
@@ -6,21 +6,28 @@ module Futu where
 import Control.Comonad.Cofree
 import Control.Monad.Free
 import Data.Functor.Foldable
+import Debug.Trace (trace)
 
-oddIndices :: [a] -> [a]
-oddIndices = histo $ \case
-  Nil                           -> []
-  Cons h (_ :< Nil)             -> [h]
-  Cons h (_ :< Cons _ (t :< _)) -> h:t
+($?) :: (Show a, Show b) => (a -> b) -> a -> b
+f $? x = let v = f x
+             msg = show x ++ " => " ++ show v
+         in trace msg v
 
-evenIndices :: [a] -> [a]
-evenIndices = histo $ \case
-  Nil                           -> []
-  Cons _ (_ :< Nil)             -> []
-  Cons _ (_ :< Cons h (t :< _)) -> h:t
 
-oddIndices' :: [a] -> [a]
-oddIndices' = futu psi where
+oddIndices :: Show a => [a] -> [a]
+oddIndices = histo (phi $?) where
+  phi Nil                             = []
+  phi (Cons h (_ :< Nil))             = [h]
+  phi (Cons h (_ :< Cons _ (t :< _))) = h:t
+
+evenIndices :: Show a => [a] -> [a]
+evenIndices = histo (phi $?) where
+  phi Nil                             = []
+  phi (Cons _ (_ :< Nil))             = []
+  phi (Cons _ (_ :< Cons h (t :< _))) = h:t
+
+oddIndices' :: Show a => [a] -> [a]
+oddIndices' = futu (psi $?) where
   psi xs = case project xs of
     Nil      -> Nil
     Cons x s -> Cons x $ do
@@ -28,8 +35,8 @@ oddIndices' = futu psi where
         Nil      -> s
         Cons _ t -> t
 
-evenIndices' :: [a] -> [a]
-evenIndices' = futu psi where
+evenIndices' :: Show a => [a] -> [a]
+evenIndices' = futu (psi $?) where
   psi xs = case project xs of
     Nil -> Nil
     Cons _ s -> case project s of
