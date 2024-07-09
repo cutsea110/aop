71d6b851dd6eb05bde72ff8ce467effc7e770b40
cutsea110 <cutsea110@gmail.com>
2024-07-10 07:24:08 +0900

    variation 2: Error with position

--- src/Interp.hs
+++ src/Interp.hs
@@ -9,10 +9,13 @@ data Term = Var Name
           | Add Term Term
           | Lam Name Term
           | App Term Term
+          | At Position Term
+
+type Position = (Int, Int)
 
 data Value = Wrong
            | Num Int
-           | Fun (Value -> M Value)
+           | Fun (Value -> P Value)
 
 type Environment = [(Name, Value)]
 
@@ -21,45 +24,50 @@ showval Wrong = "<wrong>"
 showval (Num i) = show i
 showval (Fun f) = "<fun>"
 
-interp :: Term -> Environment -> M Value
+interp :: Term -> Environment -> P Value
 interp (Var x) e = lookup x e
-interp (Con i) e = unitM (Num i)
+interp (Con i) e = unitP (Num i)
 interp (Add u v) e =
-  interp u e `bindM` (\a -> interp v e `bindM` (\b -> add a b))
-interp (Lam x v) e = unitM (Fun (\a -> interp v ((x,a):e)))
+  interp u e `bindP` (\a -> interp v e `bindP` (\b -> add a b))
+interp (Lam x v) e = unitP (Fun (\a -> interp v ((x,a):e)))
 interp (App t u) e =
-  interp t e `bindM` (\f -> interp u e `bindM` (\a -> apply f a))
+  interp t e `bindP` (\f -> interp u e `bindP` (\a -> apply f a))
+interp (At p t) e = resetP p (interp t e)
 {--
 lookup :: Name -> Environment -> M Value
 lookup x [] = unitM Wrong
 lookup x ((y,b):e) = if x == y then unitM b else lookup x e
 --}
-lookup :: Name -> Environment -> M Value
-lookup x [] = errorM ("unbound variable: " ++ x)
-lookup x ((y,b):e) = if x == y then unitM b else lookup x e
+lookup :: Name -> Environment -> P Value
+lookup x [] = errorP ("unbound variable: " ++ x)
+lookup x ((y,b):e) = if x == y then unitP b else lookup x e
 {--
 add :: Value -> Value -> M Value
 add (Num i) (Num j) = unitM (Num (i+j))
 add a       b       = unitM Wrong
 --}
-add :: Value -> Value -> M Value
-add (Num i) (Num j) = unitM (Num (i+j))
-add a       b       = errorM $ "should be numbers: " ++ showval a ++ ", " ++ showval b
+add :: Value -> Value -> P Value
+add (Num i) (Num j) = unitP (Num (i+j))
+add a       b       = errorP $ "should be numbers: " ++ showval a ++ ", " ++ showval b
 
 {--
 apply :: Value -> Value -> M Value
 apply (Fun k) a = k a
 apply f       a = unitM Wrong
 --}
-apply :: Value -> Value -> M Value
+apply :: Value -> Value -> P Value
 apply (Fun k) a = k a
-apply f       _ = errorM $ "should be function: " ++ showval f
+apply f       _ = errorP $ "should be function: " ++ showval f
 
 test :: Term -> String
-test t = showM (interp t [])
+test t = showP (interp t [])
 
 term0 :: Term
 term0 = App (Lam "x" (Add (Var "x") (Var "x"))) (Add (Con 10) (Con 11))
+
+term1 :: Term
+term1 = At (0,1) (App (At (0,2) (Lam "x" (At (1,1) (Add (At (1,2) (Var "x")) (At (2,2) (Var "x"))))))
+                   (At (0,3) (App (At (2,1) (Con 10)) (At (2,2) (Con 11)))))
 
 {--
 type M a = a
@@ -71,10 +79,29 @@ showM :: M Value -> String
 showM a = showval a
 --}
 
-data M a = Success a | Error String
-unitM a  = Success a
-errorM s = Error s
-(Success a) `bindM` k = k a
-(Error s) `bindM` k = Error s
-showM (Success a) = "Success: " ++ showval a
-showM (Error s)   = "Error: " ++ s
+data E a = Success a | Error String
+unitE a  = Success a
+errorE s = Error s
+(Success a) `bindE` k = k a
+(Error s) `bindE` k = Error s
+showE (Success a) = "Success: " ++ showval a
+showE (Error s)   = "Error: " ++ s
+
+type P a = Position -> E a
+
+unitP :: a -> P a
+unitP a = \p -> unitE a
+errorP :: String -> P a
+errorP s = \p -> errorE $ (showpos p ++  ": " ++ s)
+
+showpos :: Position -> String
+showpos p = show p
+
+bindP :: P a -> (a -> P b) -> P b
+m `bindP` k = \p -> m p `bindE` (\x -> k x p)
+
+resetP :: Position -> P a -> P a
+resetP q m = \p -> m q
+
+showP :: P Value -> String
+showP m = showE (m (0, 0))
