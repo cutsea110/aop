533f90c2f95b56237c0a5c5b1abbe9d91485a59d
cutsea110 <cutsea110@gmail.com>
2024-07-09 14:23:09 +0900

    try E: variation one

--- src/Interp.hs
+++ src/Interp.hs
@@ -12,7 +12,7 @@ data Term = Var Name
 
 data Value = Wrong
            | Num Int
-           | Fun (Value -> I Value)
+           | Fun (Value -> M Value)
 
 type Environment = [(Name, Value)]
 
@@ -21,38 +21,60 @@ showval Wrong = "<wrong>"
 showval (Num i) = show i
 showval (Fun f) = "<fun>"
 
-interp :: Term -> Environment -> I Value
+interp :: Term -> Environment -> M Value
 interp (Var x) e = lookup x e
-interp (Con i) e = unitI (Num i)
+interp (Con i) e = unitM (Num i)
 interp (Add u v) e =
-  interp u e `bindI` (\a -> interp v e `bindI` (\b -> add a b))
-interp (Lam x v) e = unitI (Fun (\a -> interp v ((x,a):e)))
+  interp u e `bindM` (\a -> interp v e `bindM` (\b -> add a b))
+interp (Lam x v) e = unitM (Fun (\a -> interp v ((x,a):e)))
 interp (App t u) e =
-  interp t e `bindI` (\f -> interp u e `bindI` (\a -> apply f a))
+  interp t e `bindM` (\f -> interp u e `bindM` (\a -> apply f a))
+{--
+lookup :: Name -> Environment -> M Value
+lookup x [] = unitM Wrong
+lookup x ((y,b):e) = if x == y then unitM b else lookup x e
+--}
+lookup :: Name -> Environment -> M Value
+lookup x [] = errorM ("unbound variable: " ++ x)
+lookup x ((y,b):e) = if x == y then unitM b else lookup x e
+{--
+add :: Value -> Value -> M Value
+add (Num i) (Num j) = unitM (Num (i+j))
+add a       b       = unitM Wrong
+--}
+add :: Value -> Value -> M Value
+add (Num i) (Num j) = unitM (Num (i+j))
+add a       b       = errorM $ "should be numbers: " ++ showval a ++ ", " ++ showval b
 
-lookup :: Name -> Environment -> I Value
-lookup x [] = unitI Wrong
-lookup x ((y,b):e) = if x == y then unitI b else lookup x e
-
-add :: Value -> Value -> I Value
-add (Num i) (Num j) = unitI (Num (i+j))
-add a       b       = unitI Wrong
-
-apply :: Value -> Value -> I Value
+{--
+apply :: Value -> Value -> M Value
 apply (Fun k) a = k a
-apply f       a = unitI Wrong
+apply f       a = unitM Wrong
+--}
+apply :: Value -> Value -> M Value
+apply (Fun k) a = k a
+apply f       _ = errorM $ "should be function: " ++ showval f
 
 test :: Term -> String
-test t = showI (interp t [])
+test t = showM (interp t [])
 
 term0 :: Term
 term0 = App (Lam "x" (Add (Var "x") (Var "x"))) (Add (Con 10) (Con 11))
 
-type I a = a
-unitI :: a -> I a
-unitI a = a
-bindI :: I a -> (a -> I b) -> I b
-a `bindI` k = k a
-showI :: I Value -> String
-showI a = showval a
+{--
+type M a = a
+unitM :: a -> M a
+unitM a = a
+bindM :: M a -> (a -> M b) -> M b
+a `bindM` k = k a
+showM :: M Value -> String
+showM a = showval a
+--}
 
+data M a = Success a | Error String
+unitM a  = Success a
+errorM s = Error s
+(Success a) `bindM` k = k a
+(Error s) `bindM` k = Error s
+showM (Success a) = "Success: " ++ showval a
+showM (Error s)   = "Error: " ++ s
