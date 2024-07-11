6d038177afc87a6dfee6c4cbbc18b37263970bc6
cutsea110 <cutsea110@gmail.com>
2024-07-12 07:21:51 +0900

    call by name for variation 0

--- src/Interp.hs
+++ src/Interp.hs
@@ -503,3 +503,48 @@ term0B :: TermB
 term0B = AppB (LamB "x" (AddB (VarB "x") (VarB "x"))) (AddB (ConB 10) (ConB 11))
 term1B :: TermB
 term1B = AddB (AddB (ConB 1) (ConB 2)) CountB
+
+
+{- | Variation 0: Standard Interpreter Call-by-Name -}
+data ValueI' = WrongI'
+             | NumI' Int
+             | FunI' (I ValueI' -> I ValueI')
+
+type EnvironmentI' = [(Name, I ValueI')]
+
+interpI' :: TermI -> EnvironmentI' -> I ValueI'
+interpI' (VarI x) e = lookupI' x e
+interpI' (ConI i) _ = unitI (NumI' i)
+interpI' (AddI u v) e = interpI' u e `bindI` (\a ->
+                        interpI' v e `bindI` (\b ->
+                        addI' a b))
+interpI' (LamI x v) e = unitI (FunI' (\a -> interpI' v ((x, a):e)))
+interpI' (AppI t u) e = interpI' t e `bindI` (\f ->
+                        applyI' f (interpI' u e))
+
+lookupI' :: Name -> EnvironmentI' -> I ValueI'
+lookupI' x [] = unitI WrongI'
+lookupI' x ((y, b) : e)
+  | x == y    = unitI b
+  | otherwise = lookupI' x e
+
+applyI' :: ValueI' -> ValueI' -> I ValueI'
+applyI' (FunI' k) a = k a
+applyI' f         _ = unitI WrongI'
+
+addI' :: ValueI' -> ValueI' -> I ValueI'
+addI' (NumI' i) (NumI' j) = unitI (NumI' (i+j))
+addI' a        b          = unitI WrongI'
+
+showI' :: I ValueI' -> String
+showI' WrongI' = "<wrong>"
+showI' (NumI' i) = show i
+showI' (FunI' _) = "<function>"
+
+testI' :: TermI -> String
+testI' t = showI' (interpI' t [])
+
+term0I' :: TermI
+term0I' = AppI (LamI "x" (AddI (VarI "x") (VarI "x"))) (AddI (ConI 10) (ConI 11))
+
+
