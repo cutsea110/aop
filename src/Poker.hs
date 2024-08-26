44052248e5be4567d5a4bcf141bbd5e30deaf70b
cutsea110 <cutsea110@gmail.com>
2024-08-27 07:37:07 +0900

    calculate turnsAndRivers

--- src/Poker.hs
+++ src/Poker.hs
@@ -7,4 +7,14 @@ turnRate :: Outs -> Float
 turnRate outs = fromIntegral outs / 47 * 100
 
 riverRate :: Outs -> Float
-riverRate outs = (1 - (47 - fromIntegral outs)/47 * (46 - fromIntegral outs)/46) * 100
+riverRate outs = (1 - f 47 * f 46) * 100
+  where
+    f n = 1 - fromIntegral outs / n
+
+
+turnsAndRivers :: [(Float, Float)]
+turnsAndRivers = map (\i -> pair round2 (turnRate i, riverRate i)) [1..20]
+  where
+    pair f (x, y) = (f x, f y)
+    round2 :: Float -> Float
+    round2 = (/ 100) . fromIntegral . round . (* 100)
