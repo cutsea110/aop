272fc0e6d627325d978327a99d87b0dae1598369
cutsea110 <cutsea110@gmail.com>
2024-08-27 07:38:52 +0900

    refactor

--- src/Poker.hs
+++ src/Poker.hs
@@ -2,6 +2,8 @@ module Poker where
 
 type Outs = Int
 
+pair :: (a -> b) -> (a, a) -> (b, b)
+pair f (x, y) = (f x, f y)
 
 turnRate :: Outs -> Float
 turnRate outs = fromIntegral outs / 47 * 100
@@ -15,6 +17,5 @@ riverRate outs = (1 - f 47 * f 46) * 100
 turnsAndRivers :: [(Float, Float)]
 turnsAndRivers = map (\i -> pair round2 (turnRate i, riverRate i)) [1..20]
   where
-    pair f (x, y) = (f x, f y)
     round2 :: Float -> Float
     round2 = (/ 100) . fromIntegral . round . (* 100)
