074d7f079ca13b1a4c870a1fa3f1369197aef46e
cutsea110 <cutsea110@gmail.com>
2024-08-28 08:05:14 +0900

    refactor

--- src/Poker.hs
+++ src/Poker.hs
@@ -4,6 +4,9 @@ import Control.Arrow
 
 type Outs = Int
 
+roundN :: Int -> Float -> Float
+roundN n = (/ d) . fromIntegral . round . (* d) where d = 10 ^ n
+
 -- | フロップ時点での outs 数から算出されるターン時の勝率
 turnRate :: Outs -> Float
 turnRate outs = fromIntegral outs / 47 * 100
@@ -11,12 +14,9 @@ turnRate outs = fromIntegral outs / 47 * 100
 -- | フロップ時点での outs 数から算出されるリバーまで含めた勝率
 riverRate :: Outs -> Float
 riverRate outs = (1 - f 47 * f 46) * 100
-  where
-    f n = 1 - fromIntegral outs / n
+  where f n = 1 - fromIntegral outs / n
 
 -- | アウツが 1 から 20 のときのターンとリバーまでの勝率
 turnsAndRivers :: [(Float, Float)]
 turnsAndRivers = map (round2 . turnRate &&& round2 . riverRate) [1..20]
-  where
-    round2 :: Float -> Float
-    round2 = (/ 100) . fromIntegral . round . (* 100)
+  where round2 = roundN 2
