ef04f8a199c2708e7a00b02ab73fa7305df0ad53
cutsea110 <cutsea110@gmail.com>
2024-08-29 21:01:37 +0900

    basic statistical operator

--- src/Poker.hs
+++ src/Poker.hs
@@ -20,3 +20,14 @@ riverRate outs = (1 - f 47 * f 46) * 100
 turnsAndRivers :: [(Float, Float)]
 turnsAndRivers = map (round2 . turnRate &&& round2 . riverRate) [1..20]
   where round2 = roundN 2
+
+-- | https://ja.pokerstrategy.com/strategy/various-poker/%E3%83%86%E3%82%AD%E3%82%B5%E3%82%B9%E3%83%9B%E3%83%BC%E3%83%AB%E3%83%87%E3%83%A0-%E7%A2%BA%E7%8E%87/
+--
+fact :: Integer -> Integer
+fact n = product [1..n]
+
+perm :: Integer -> Integer -> Integer
+perm n k = fact n `div` fact (n - k)
+
+comb :: Integer -> Integer -> Integer
+comb n k = perm n k `div` fact k
