bd06a8c6611685eae6f7d3cb4aaf380ab1403405
cutsea110 <cutsea110@gmail.com>
2024-10-01 12:31:45 +0900

    digits implemented by using catamorphism

--- src/NumberCard.hs
+++ src/NumberCard.hs
@@ -5,14 +5,16 @@ module NumberCard where
 --  1. カード [1]  はじまりの digit 文字列
 --  2. カード [2]  はじまりの digit 文字列
 --  3. カード [13] はじまりの digit 文字列
-digits :: Int -> [[String]]
-digits 0 = [[],[],[""]] -- "" はどこでも良いが1つだけ入れておく
-digits 1 = [["1"], ["2"], []]
-digits (n+2) = [prefix1', prefix2', prefix13']
-  where ([prefix1, prefix2, prefix13], pred2) = (digits (n+1), digits n)
-        prefix1'  = map ("1"++) $ prefix1 ++ prefix2 ++ prefix13
-        prefix2'  = map ("2"++) $ prefix1 ++ prefix13
-        prefix13' = map ("13"++) $ concat pred2
+digits :: Integer -> [String]
+digits = concat3 . snd . foldn (c, f) . toNat
+  where
+    concat3 (x, y, z) = x ++ y ++ z
+    c = ((["1"], ["2"], []), ([], [], [""]))
+    f (pred@(p1, p2, p13), (q1, q2, q3)) = ((p1', p2', p13'), pred)
+      where p1'  = map ("1"++) $ p1 ++ p2 ++ p13
+            p2'  = map ("2"++) $ p1 ++ p13
+            p13' = map ("13"++) $ q1 ++ q2 ++ q3
+
 
 data Nat = Z | S Nat deriving Show
 
