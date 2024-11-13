a1a2a922d420fad6ca5885bb69e83338b8744cc7
cutsea110 <cutsea110@gmail.com>
2024-11-13 17:50:46 +0900

    update MSS

--- src/MSS.hs
+++ src/MSS.hs
@@ -4,5 +4,24 @@ subseqs :: [a] -> [[a]]
 subseqs = foldr op [[]]
   where op x xss = xss ++ map (x:) xss
 
-mss :: [Integer] -> Integer
-mss = foldr op 0 where op x y = y `max` (x + y)
+inits :: [a] -> [[a]]
+inits = foldr op [[]]
+  where op x xss = [] : map (x:) xss
+
+tails :: [a] -> [[a]]
+tails = foldr op [[]]
+  where op x xss = (x : head xss) : xss
+
+mss1 :: [Integer] -> Integer
+mss1 = maximum . map sum . segments
+  where segments = concatMap inits . tails
+
+mss2 :: [Integer] -> Integer
+mss2 = maximum . map (maximum . f) . tails
+  where f = foldr op [0]
+          where op x y = map (x+) y ++ [0]
+
+mss3 :: [Integer] -> Integer
+mss3 = maximum . map f . tails
+  where f = foldr oplus 0
+          where oplus x y = max (x+y) 0
