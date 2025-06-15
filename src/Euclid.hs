214ddf3095649f4b0eaee729e4e6fd6fdff8a272
cutsea110 <cutsea110@gmail.com>
2025-06-15 22:02:44 +0900

    add Euclid

--- src/Euclid.hs
+++ src/Euclid.hs
@@ -1,1 +1,20 @@
+{-# LANGUAGE NPlusKPatterns, TupleSections #-}
 module Euclid where
+
+euclid :: Integral a => a -> a -> a
+euclid x 0 = x
+euclid x y = euclid y (x `mod` y)
+
+cata :: Integral b => a -> (a -> a) -> b -> a
+cata c f = u
+  where u 0       = c
+        u (n + 1) = f (u n)
+
+euclid' :: Integral a => a -> a -> a
+euclid' x y = v $ pure (x, y)
+  where
+    v = either id (v . psi)
+      where
+        psi (x, y) = case x `mod` y of
+          0 -> Left y
+          m -> Right (y, m)
