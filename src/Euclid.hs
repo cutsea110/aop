af41ef693bd95719102f97ce621b0028af56838a
cutsea110 <cutsea110@gmail.com>
2025-06-16 09:33:48 +0900

    refine

--- src/Euclid.hs
+++ src/Euclid.hs
@@ -5,11 +5,6 @@ euclid :: Integral a => a -> a -> a
 euclid x 0 = x
 euclid x y = euclid y (x `mod` y)
 
-cata :: Integral b => a -> (a -> a) -> b -> a
-cata c f = u
-  where u 0       = c
-        u (n + 1) = f (u n)
-
 {-
 euclid' :: Integral a => a -> a -> a
 euclid' x y = v $ pure (x, y)
@@ -23,21 +18,21 @@ euclid' x y = v $ pure (x, y)
 
 data NEL a = Unit a | Cons a (NEL a) deriving (Show, Functor)
 
-foldNEL :: (a -> b, (a, b) -> b) -> NEL a -> b
-foldNEL (f, g) = u
+cata :: (a -> b, (a, b) -> b) -> NEL a -> b
+cata (f, g) = u
   where
     u (Unit x)    = f x
     u (Cons x xs) = g (x, u xs)
 
-unfoldNEL :: (b -> Either a (a, b)) -> b -> NEL a
-unfoldNEL psi = v
+ana :: (b -> Either a (a, b)) -> b -> NEL a
+ana psi = v
   where
     v b = case psi b of
       Left x       -> Unit x
       Right (x, y) -> Cons x (v y)
 
 euclid' :: Integral a => (a, a) -> a
-euclid' = foldNEL phi . unfoldNEL psi
+euclid' = cata phi . ana psi
   where
     phi = (id, snd)
     psi (x, 0) = Left x
