dd70b4b40508af6725a553d7a06a73b459c470df
cutsea110 <cutsea110@gmail.com>
2025-07-10 10:30:54 +0900

    ana version

--- src/RLE.hs
+++ src/RLE.hs
@@ -2,26 +2,11 @@
 {-# LANGUAGE DeriveFunctor #-}
 module RLE where
 
-import Data.Fix (Fix(..))
-import Control.Monad.Free (Free(..), liftF)
-import Data.Functor.Foldable (futu, cata, ListF(..))
+import Data.List (unfoldr)
 
--- coalgebra
-rleCoalg :: String -> ListF (Char, Int) (Free (ListF (Char, Int)) String)
-rleCoalg "" = Nil
-rleCoalg (c:cs) = Cons (c, 1+length same) $ return rest
-  where (same, rest) = span (==c) cs
-
--- encode
-toRLE :: String -> Fix (ListF (Char, Int))
-toRLE = futu rleCoalg
-
--- decode
-fromRLE :: Fix (ListF (Char, Int)) -> [(Char, Int)]
-fromRLE = cata phi
+toRLE :: Eq a => [a] -> [(a, Int)]
+toRLE = unfoldr psi
   where
-    phi Nil         = []
-    phi (Cons x xs) = x:xs
-
-test :: String -> [(Char, Int)]
-test = fromRLE . toRLE
+    psi [] = Nothing
+    psi xxs@(x:xs) = Just ((x, length ys), zs)
+      where (ys, zs) = span (== x) xxs
