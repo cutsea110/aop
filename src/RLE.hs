33404e62ac6b94ed827339dcb47e348fafd9ed4d
cutsea110 <cutsea110@gmail.com>
2025-07-10 10:59:03 +0900

    toRLE defined by using ana and the co-algebra is defined by para

--- src/RLE.hs
+++ src/RLE.hs
@@ -4,9 +4,18 @@ module RLE where
 
 import Data.List (unfoldr)
 
+para :: b -> (a -> ([a], b) -> b) -> [a] -> b
+para c f = u
+  where u [] = c
+        u (x:xs) = f x (xs, u xs)
+
 toRLE :: Eq a => [a] -> [(a, Int)]
 toRLE = unfoldr psi
   where
-    psi [] = Nothing
-    psi xxs@(x:xs) = Just ((x, length ys), zs)
-      where (ys, zs) = span (== x) xxs
+    psi :: Eq a => [a] -> Maybe ((a, Int), [a])
+    psi = para Nothing f
+      where
+        f x (xs, Nothing) = Just ((x, 1), xs)
+        f x (xs, Just ((y, n), zs))
+          | x == y   = Just ((y, succ n), zs)
+          | otherwise = Just ((x, 1), xs)
