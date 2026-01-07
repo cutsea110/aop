2980f310d5e3b591e2075864aa4fa27af4c8fc8b
cutsea110 <cutsea110@gmail.com>
2026-01-07 09:33:05 +0900

    refactor

--- src/YearFloat.hs
+++ src/YearFloat.hs
@@ -2,22 +2,20 @@ module YearFloat where
 
 import Data.Ratio
 
-x :: Rational
-x = (2026 + 1 % 9999) / 9999
+y2026 :: Rational
+y2026 = (2026 + 1 % 9999) / 9999
 
 decimalDigits :: Rational -> [Int]
 decimalDigits r = go (numerator r `mod` denominator r)
-  where
-    d = denominator r
-    go rem =
-      let (q, rem') = (rem * 10) `divMod` d
-      in fromIntegral q : go rem'
+  where d = denominator r
+        go rem = fromIntegral q : go rem'
+          where (q, rem') = (rem * 10) `divMod` d
+
 
 showDecimal :: Int -> Rational -> String
-showDecimal n r =
-  let i = numerator r `div` denominator r
-      ds = decimalDigits r
-  in show i ++ "." ++ concatMap show (take n ds)
+showDecimal n r = show i ++ "." ++ concatMap show (take n ds)
+  where i = numerator r `div` denominator r
+        ds = decimalDigits r
 
 main :: IO ()
-main = putStrLn $ showDecimal 1000 x
+main = putStrLn $ showDecimal 1000 y2026
